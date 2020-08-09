8141405688c5102e9d462e41a47da6faaeddeb20
Whitney Armstrong <warmstrong@anl.gov>
2020-08-09 18:06:43 -0500

    new file:   tests/options/simple_reader.py

---
+++ JugBase/tests/options/simple_reader.py
@@ -0,0 +1,24 @@
+from Gaudi.Configuration import *
+
+from Configurables import ApplicationMgr, EICDataSvc, PodioOutput
+
+podioevent   = EICDataSvc("EventDataSvc", inputs=["derp.root"], OutputLevel=DEBUG)
+
+# reads HepMC text file and write the HepMC::GenEvent to the data service
+from Configurables import PodioInput #, ReadTestConsumer
+podioinput = PodioInput("PodioReader", collections=["mcparticles"], OutputLevel=DEBUG)
+#checker = ReadTestConsumer()
+
+out = PodioOutput("out", filename="test.root")
+out.outputCommands = ["keep *"]
+
+ApplicationMgr(
+    TopAlg = [podioinput, #checker,
+              out
+              ],
+    EvtSel = 'NONE',
+    EvtMax   = 5,
+    ExtSvc = [podioevent],
+    OutputLevel=DEBUG
+ )
+
