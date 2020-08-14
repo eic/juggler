91098855c3dee72bbdc92c6d7e8d193bcce19f22
Whitney Armstrong <warmstrong@anl.gov>
2020-08-14 00:42:00 -0500

    new file:   TestACTSLogger.cpp 	new file:   TestACTSLogger.h 	new file:   TestCKFTracks.h

---
+++ JugReco/src/components/TestCKFTracks.h
@@ -0,0 +1,34 @@
+#ifndef TESTRECONSTRUCTION_TESTACTSLOGGER_H
+#define TESTRECONSTRUCTION_TESTACTSLOGGER_H
+
+// GAUDI
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/Property.h"
+
+// FCCSW
+#include "JugBase/DataHandle.h"
+
+
+class TestCKFTracks : public GaudiAlgorithm {
+public:
+
+  explicit TestCKFTracks(const std::string&, ISvcLocator*);
+  virtual ~TestCKFTracks();
+
+  /**  Initialize.
+   *   @return status code
+   */
+  virtual StatusCode initialize() final;
+
+  /**  Execute.
+   *   @return status code
+   */
+  virtual StatusCode execute() final;
+
+  /**  Finalize.
+   *   @return status code
+   */
+  virtual StatusCode finalize() final;
+
+};
+#endif /* TESTRECONSTRUCTION_TESTACTSLOGGER_H */
