719ae83ec548c39f13afdcd2ebe38eac906523c3
jihee.kim <jihee.kim@anl.gov>
2020-09-22 22:20:13 -0500

    Modified python code for crystal digitization

--- JugDigi/tests/options/example_crystalendcapsdigi.py
+++ JugDigi/tests/options/example_crystalendcapsdigi.py
@@ -4,21 +4,21 @@ from GaudiKernel.DataObjectHandleBase import DataObjectHandleBase
 from Configurables import ApplicationMgr, EICDataSvc, PodioOutput, GeoSvc
 
 geo_service  = GeoSvc("GeoSvc")
-podioevent   = EICDataSvc("EventDataSvc", inputs=["output_emcal_electrons.root"], OutputLevel=DEBUG)
+podioevent   = EICDataSvc("EventDataSvc", inputs=["output_emcal_electrons_npsim.root"], OutputLevel=DEBUG)
 
 from Configurables import PodioInput
 from Configurables import Jug__Digi__CrystalEndcapsDigi as CrystalEndcapsDigi
 
-podioinput = PodioInput("PodioReader", collections=["MCParticles","EcalHits"], OutputLevel=DEBUG)
+podioinput = PodioInput("PodioReader", collections=["mcparticles","EcalHits"], OutputLevel=DEBUG)
 emcaldigi = CrystalEndcapsDigi(inputHitCollection="EcalHits",outputHitCollection="RawDigiEcalHits")
 
-out = PodioOutput("out", filename="digi_emcal_electrons.root")
+out = PodioOutput("out", filename="digi_emcal_electrons_npsim.root")
 out.outputCommands = ["keep *"]
 
 ApplicationMgr(
     TopAlg = [podioinput, emcaldigi, out],
     EvtSel = 'NONE',
-    EvtMax   = 5,
+    EvtMax   = 100,
     ExtSvc = [podioevent],
     OutputLevel=DEBUG
  )
