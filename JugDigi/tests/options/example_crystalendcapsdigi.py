19563a767d39448c63b31a294264c45df8899a9d
Whitney Armstrong <warmstrong@anl.gov>
2020-10-05 18:36:46 -0500

    Turning back tree name to "events"
    - Not sure why it was changed to "EVENT".
    - This name is set by podio.

--- JugDigi/tests/options/example_crystalendcapsdigi.py
+++ JugDigi/tests/options/example_crystalendcapsdigi.py
@@ -3,14 +3,14 @@ from Gaudi.Configuration import *
 from GaudiKernel.DataObjectHandleBase import DataObjectHandleBase
 from Configurables import ApplicationMgr, EICDataSvc, PodioOutput, GeoSvc
 
-geo_service  = GeoSvc("GeoSvc")
-podioevent   = EICDataSvc("EventDataSvc", inputs=["output_emcal_electrons_npsim.root"], OutputLevel=DEBUG)
-
 from Configurables import PodioInput
 from Configurables import Jug__Digi__CrystalEndcapsDigi as CrystalEndcapsDigi
 
+#geo_service  = GeoSvc("GeoSvc")
+podioevent   = EICDataSvc("EventDataSvc", inputs=["output_emcal_electrons_npsim.root"], OutputLevel=DEBUG)
+
 podioinput = PodioInput("PodioReader", collections=["mcparticles","EcalHits"], OutputLevel=DEBUG)
-emcaldigi = CrystalEndcapsDigi(inputHitCollection="EcalHits",outputHitCollection="RawDigiEcalHits")
+emcaldigi = CrystalEndcapsDigi("ecal_digi",inputHitCollection="EcalHits",outputHitCollection="RawDigiEcalHits", OutputLevel=DEBUG)
 
 out = PodioOutput("out", filename="digi_emcal_electrons_npsim.root")
 out.outputCommands = ["keep *"]
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Project Juggler
+Project Juggler
+===============
 
 Concurrent event processor for NP experiments, based on the Gaudi framework.
+
+Overview
+--------
+
+### Running Juggler
+
+```
+./scripts/run_topside.py -i eic_upsilon.hepmc -o test_hytop.root -n 10
+../where_ever/../juggler/build/run gaudirun.py options/example_reconstruction.py
+```
+
+
+
+### `JugBase`
+
+### `JugDigi`
+
+### `JugReco`
+
+
+
