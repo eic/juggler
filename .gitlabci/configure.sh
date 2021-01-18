1c3344a5d054b2f4df891933574f8465dd5392bb
Whitney Armstrong <warmstrong@anl.gov>
2021-01-18 00:36:08 -0600

    modified:   .containers/Dockerfile.in 	modified:   .gitlabci/configure.sh

--- .gitlabci/configure.sh
+++ .gitlabci/configure.sh
@@ -6,9 +6,15 @@
 ## Known variables that will be substituted:
 ## @EIC_TAG@             - docker tag used for the EIC container
 ## @JUGGLER_TAG@         - output tag for the Juggler version
+## @JUGGLER_BRANCH@      - Juggler git branch for build
 
 TEMPLATE_FILE=$1
 OUTPUT_FILE=${TEMPLATE_FILE%.*}
+JUGGLER_BRANCH="master"
+
+if [ -n "${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" ] ; then
+  JUGGLER_BRANCH=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+fi
 
 echo "Configuring CI file: ${TEMPLATE_FILE}"
 echo "Output will be written to: ${OUTPUT_FILE}"
@@ -16,6 +22,8 @@ echo "Output will be written to: ${OUTPUT_FILE}"
 TAG=$EIC_VERSION
 
 sed "s/@EIC_TAG@/$EIC_TAG/g" $TEMPLATE_FILE | \
-  sed "s/@JUGGLER_TAG@/$JUGGLER_TAG/g" > ${OUTPUT_FILE}
+  sed "s/@JUGGLER_TAG@/$JUGGLER_TAG/g" | \
+  sed "s/@JUGGLER_BRANCH@/$JUGGLER_BRANCH/g" > ${OUTPUT_FILE}
+
 
 echo "Done"
