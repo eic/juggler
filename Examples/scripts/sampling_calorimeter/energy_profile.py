8d3b16b99a7c2221b1db31506628d7a801950754
Chao Peng <cpeng@anl.gov>
2021-05-04 15:45:23 +0000

    Revamp analysis scripts for benchmarks

--- Examples/scripts/sampling_calorimeter/energy_profile.py
+++ Examples/scripts/sampling_calorimeter/energy_profile.py
@@ -1,3 +1,12 @@
+'''
+    A script to generate the energy profile (layer-wise)
+    It reads the output from the Juggler component ImagingClusterReco, which is supposed to be clusters of hits after
+    digitization, reconstruction, and clustering
+
+    Author: Chao Peng (ANL)
+    Date: 04/30/2021
+'''
+
 import os
 import numpy as np
 import pandas as pd
@@ -8,118 +17,113 @@ import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator, MaxNLocator
 import sys
-
-
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
-
-
-# define Ecal shapes
-rmin, thickness, length = 890, 20*(10. + 1.65), 860*2+500
-
-parser = argparse.ArgumentParser(description='sampling calorimeter energy profiling')
-parser.add_argument('file', type=str, help='path to root file')
-parser.add_argument('--plot-dir', type=str, default='./plots', dest='outdir', help='output directory')
-parser.add_argument('--save-prof', type=str, default='', dest='save', help='path to save profile')
-parser.add_argument('--color', type=str, default='royalblue', dest='color', help='colors for bar plots')
-args = parser.parse_args()
-
-os.makedirs(args.outdir, exist_ok=True)
-
-gROOT.Macro('rootlogon.C')
-f = ROOT.TFile(args.file)
-events = f.events
-
-# prepare data
-dfe = pd.DataFrame()
-for iev in np.arange(events.GetEntries()):
-    events.GetEntry(iev)
-    data = []
-    for hit in events.RecoEcalBarrelHits:
-        lid = (hit.cellID >> 15) & 1023
-        data.append((lid, hit.position.x, hit.position.y, hit.position.z, hit.energy))
-    df = pd.DataFrame(data=data, columns=['id', 'x', 'y', 'z', 'edep'])
-    dft = df.groupby('id')['edep'].sum().to_frame()
-    dft.loc[:, 'event'] = iev
-    dfe = pd.concat([dfe, dft])
-    # mc_p = (events.mcparticles2[2].psx, events.mcparticles2[2].psy, events.mcparticles2[2].psz)
-dfe.reset_index(inplace=True)
-# print(dfe)
+from utils import *
 
 
 def find_start_layer(grp, min_edep=0.5):
-    ids, edeps = grp.sort_values('id')[['id', 'edep']].values.T
+    ids, edeps = grp.sort_values('layer')[['layer', 'edep']].values.T
     edeps = np.cumsum(edeps)
     return min(ids[edeps > min_edep]) if sum(edeps > min_edep) > 0 else 20
 
 
-slayer = dfe.groupby('event').apply(find_start_layer).astype(int)
-dfe = dfe.merge(slayer.to_frame(name='slayer'), on='event')
-# dfe.loc[:, 'cid'] = dfe['id'] - dfe['slayer']
-# prof = dfe[dfe['cid'] > 0].groupby('cid')['edep'].describe()
-prof = dfe.groupby('id')['edep'].describe()
-# print(prof['mean'])
-bpos, bprops = (0.5, 0.95), dict(boxstyle='round', facecolor='wheat', alpha=0.3)
+# execute this script
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate energy profiles')
+    parser.add_argument('file', type=str, help='path to root file')
+    parser.add_argument('--plot-dir', type=str, default='./plots', dest='outdir', help='output directory')
+    parser.add_argument('--type', type=str, default='unknown', dest='type', help='profile type (used in save)')
+    parser.add_argument('--energy', type=float, default=5000., dest='energy', help='incident particle energy (MeV)')
+    parser.add_argument('--save', type=str, default='', dest='save', help='path to save profile')
+    parser.add_argument('--color', type=str, default='royalblue', dest='color', help='colors for bar plots')
+    parser.add_argument('-b', '--branch-name', type=str, default='EcalBarrelClustersLayers', dest='branch',
+                        help='branch name in the root file (outputLayerCollection from ImagingClusterReco)')
+    parser.add_argument('-m', '--macros', type=str, default='rootlogon.C', dest='macros',
+                        help='root macros to load (accept multiple paths separated by \",\")')
+    args = parser.parse_args()
 
+    load_root_macros(args.macros)
+    # prepare data
+    dfe = get_layers_data(args.file, branch=args.branch)
+    dfe.loc[:, 'total_edep'] = dfe.groupby(['event', 'cluster'])['edep'].transform('sum')
+    # dfe.loc[:, 'efrac'] = dfe['edep']/dfe['total_edep']
+    dfe.loc[:, 'efrac'] = dfe['edep']/args.energy
+    dfe = dfe[dfe['cluster'] == 0]
 
-fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
-ax.hist(dfe.groupby('event')['slayer'].min(), weights=[1/float(events.GetEntries())]*events.GetEntries(),
-        ec='black', bins=np.arange(0.5, 10.5, step=1.0))
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-ax.xaxis.set_minor_locator(MultipleLocator(1))
-ax.yaxis.set_minor_locator(MultipleLocator(1))
-ax.grid(linestyle=':', which='both')
-ax.tick_params(labelsize=24)
-ax.set_xlabel('Start Layer', fontsize=26)
-ax.set_ylabel('Normalized Counts', fontsize=26)
-ax.text(*bpos, 'Mininum Edep\n' + '{:.1f} MeV'.format(1.0),
-        transform=ax.transAxes, fontsize=26, verticalalignment='top', bbox=bprops)
-fig.savefig(os.path.join(args.outdir, 'edep_start.png'))
+    os.makedirs(args.outdir, exist_ok=True)
 
+    slayer = dfe.groupby('event').apply(lambda x: find_start_layer(x, 1.0)).astype(int)
+    dfe = dfe.merge(slayer.to_frame(name='slayer'), on='event')
+    # dfe.loc[:, 'eff_layer'] = dfe['layer'] - dfe['slayer']
+    # prof = dfe[dfe['eff_layer'] > 0].groupby('eff_layer')['edep'].describe()
+    prof = dfe.groupby('layer')['efrac'].describe()
 
-fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
-ax.plot(prof.index, prof['mean'], color=args.color, lw=2)
-# ax.fill_between(prof.index, prof['25%'], prof['75%'], color=args.color, alpha=0.3)
-ax.fill_between(prof.index, prof['mean'] - prof['std'], prof['mean'] + prof['std'], color=args.color, alpha=0.3)
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-ax.xaxis.set_minor_locator(MultipleLocator(1))
-ax.yaxis.set_minor_locator(MultipleLocator(1))
-ax.grid(linestyle=':', which='both')
-ax.tick_params(labelsize=24)
-ax.set_xlabel('Layer', fontsize=26)
-ax.set_ylabel('Energy Deposit (MeV)', fontsize=26)
-fig.savefig(os.path.join(args.outdir, 'edep.png'))
-
-layers = np.asarray([
-    [1, 5, 8,],
-    [10, 15, 20],
-])
-
-fig, ax = plt.subplots(*layers.shape, figsize=(16, 9), dpi=160, sharex='col', sharey='all',
-                       gridspec_kw=dict(hspace=0.05, wspace=0.05))
-
-for ax, layer in zip(ax.flat, layers.flatten()):
-    data = dfe[dfe['id'] == layer]
-    ax.hist(data['edep'], weights=[1/float(len(data))]*len(data), bins=np.linspace(0, 20, 40),
-            ec='black', color=args.color)
-    ax.tick_params(labelsize=24)
+    # print(prof['mean'])
+    # plot profiles
+    bpos, bprops = (0.5, 0.95), dict(boxstyle='round', facecolor='wheat', alpha=0.3)
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
+    nev = len(dfe['event'].unique())
+    ax.hist(dfe.groupby('event')['slayer'].min(), weights=[1/float(nev)]*nev,
+            ec='black', bins=np.arange(0.5, 10.5, step=1.0))
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
     ax.grid(linestyle=':', which='both')
-    # ax.set_xlabel('Energy Deposit (MeV)', fontsize=26)
-    # ax.set_ylabel('Normalized Counts', fontsize=26)
-    mean, std = data.describe().loc[['mean', 'std'], 'edep'].values
-    label = 'Layer {}'.format(layer)
-#            + '\n' + r'$\mu = {:.3f}$ MeV'.format(mean)
-#            + '\n' + r'$\sigma = {:.3f}$ MeV'.format(std)
-    ax.text(*bpos, label, transform=ax.transAxes, fontsize=26, verticalalignment='top', bbox=bprops)
-fig.text(0.5, 0.02, 'Energy Deposit (MeV)', fontsize=26, ha='center')
-fig.text(0.02, 0.5, 'Normalized Counts', fontsize=26, va='center', rotation=90)
-fig.savefig(os.path.join(args.outdir, 'edep_layers.png'))
+    ax.tick_params(labelsize=24)
+    ax.set_xlabel('Start Layer', fontsize=26)
+    ax.set_ylabel('Normalized Counts', fontsize=26)
+    ax.text(*bpos, 'Mininum Edep\n' + '{:.1f} MeV'.format(1.0),
+            transform=ax.transAxes, fontsize=26, verticalalignment='top', bbox=bprops)
+    fig.savefig(os.path.join(args.outdir, 'edep_start.png'))
 
 
-if args.save:
-    prof.to_csv(args.save)
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
+    ax.plot(prof.index, prof['mean'].values*100., color=args.color, lw=2)
+    # ax.fill_between(prof.index, prof['25%'], prof['75%'], color=args.color, alpha=0.3)
+    ax.fill_between(prof.index, (prof['mean'] - prof['std'])*100., (prof['mean'] + prof['std'])*100.,
+                    color=args.color, alpha=0.3)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
+    ax.grid(linestyle=':', which='both')
+    ax.tick_params(labelsize=24)
+    ax.set_xlabel('Layer', fontsize=26)
+    ax.set_ylabel('Energy Deposit Percentage', fontsize=26)
+    fig.savefig(os.path.join(args.outdir, 'efrac.png'))
+
+    layers = np.asarray([
+        [1, 5, 8,],
+        [10, 15, 20],
+    ])
+
+    fig, ax = plt.subplots(*layers.shape, figsize=(16, 9), dpi=160, sharex='col', sharey='all',
+                           gridspec_kw=dict(hspace=0.05, wspace=0.05))
+
+    for ax, layer in zip(ax.flat, layers.flatten()):
+        data = dfe[dfe['layer'] == layer]
+        ax.hist(data['efrac'].values*100., weights=[1/float(len(data))]*len(data), bins=np.linspace(0, 30, 60),
+                ec='black', color=args.color)
+        ax.tick_params(labelsize=24)
+        ax.xaxis.set_minor_locator(MultipleLocator(1))
+        ax.grid(linestyle=':', which='both')
+        # ax.set_xlabel('Energy Deposit (MeV)', fontsize=26)
+        # ax.set_ylabel('Normalized Counts', fontsize=26)
+        mean, std = data.describe().loc[['mean', 'std'], 'edep'].values
+        label = 'Layer {}'.format(layer)
+    #            + '\n' + r'$\mu = {:.3f}$ MeV'.format(mean)
+    #            + '\n' + r'$\sigma = {:.3f}$ MeV'.format(std)
+        ax.text(*bpos, label, transform=ax.transAxes, fontsize=26, verticalalignment='top', bbox=bprops)
+        ax.set_axisbelow(True)
+        ax.set_yscale('log')
+    fig.text(0.5, 0.02, 'Energy Deposit Percentage', fontsize=26, ha='center')
+    fig.text(0.02, 0.5, 'Normalized Counts', fontsize=26, va='center', rotation=90)
+    fig.savefig(os.path.join(args.outdir, 'efrac_layers.png'))
+
+
+    if args.save:
+        prof.loc[:, 'energy'] = args.energy
+        prof.loc[:, 'type'] = args.type
+        if os.path.exists(args.save):
+            prev = pd.read_csv(args.save).set_index('layer', drop=True)
+            prof = pd.concat([prof, prev])
+        prof.to_csv(args.save)
 
--- Examples/scripts/sampling_calorimeter/epi_separation.py
+++ Examples/scripts/sampling_calorimeter/epi_separation.py
@@ -1,3 +1,12 @@
+'''
+    A script to use the energy profile for e-pi separation
+    It reads the output from the Juggler component ImagingClusterReco, which is supposed to be clusters of hits after
+    digitization, reconstruction, and clustering
+
+    Author: Chao Peng (ANL)
+    Date: 04/30/2021
+'''
+
 import os
 import numpy as np
 import pandas as pd
@@ -8,95 +17,84 @@ import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator, MaxNLocator
 import sys
+from utils import *
 
 
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
+def prepare_data(path, **kwargs):
+    tmp = get_layers_data(path, **kwargs)
+    tmp.loc[:, 'total_edep'] = tmp.groupby(['event', 'cluster'])['edep'].transform('sum')
+    tmp.loc[:, 'efrac'] = tmp['edep']/tmp['total_edep']
+    # tmp = tmp[tmp['cluster'] == 0]
+    return tmp
 
 
-# define Ecal shapes
-rmin, thickness, length = 890, 20*(10. + 1.65), 860*2+500
-
-parser = argparse.ArgumentParser(description='epi_separation')
-parser.add_argument('efile', type=str, help='path to root file (electrons)')
-parser.add_argument('pifile', type=str, help='path to root file (pions)')
-parser.add_argument('--e-prof', type=str, default='e_prof_5gev.csv', help='path to electron profile')
-parser.add_argument('--pi-prof', type=str, default='pi_prof_5gev.csv', help='path to electron profile')
-parser.add_argument('--plot-dir', type=str, default='./plots', dest='outdir', help='output directory')
-args = parser.parse_args()
-
-os.makedirs(args.outdir, exist_ok=True)
-
-
-def get_data(path):
-    f = ROOT.TFile(path)
-    events = f.events
-    # prepare data
-    df1 = pd.DataFrame()
-    for iev in np.arange(events.GetEntries()):
-        events.GetEntry(iev)
-        data = []
-        for hit in events.RecoEcalBarrelHits:
-            lid = (hit.cellID >> 15) & 1023
-            data.append((lid, hit.position.x, hit.position.y, hit.position.z, hit.energy))
-        df2 = pd.DataFrame(data=data, columns=['id', 'x', 'y', 'z', 'edep'])
-        dft = df2.groupby('id')['edep'].sum().to_frame()
-        dft.loc[:, 'event'] = iev
-        df1 = pd.concat([df1, dft])
-        # mc_p = (events.mcparticles2[2].psx, events.mcparticles2[2].psy, events.mcparticles2[2].psz)
-    df1.reset_index(inplace=True)
-    return df1
-
-
-profe = pd.read_csv(args.e_prof).set_index('id', drop=True)
-profpi = pd.read_csv(args.pi_prof).set_index('id', drop=True)
-dfe = get_data(args.efile)
-dfpi = get_data(args.pifile)
-
-colors = ['royalblue', 'indianred']
-
-energy = 5000.
-fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
-for prof, color, title in zip([profe, profpi], colors, ['electron', 'pion']):
-    ax.plot(prof.index, prof['mean']/energy*100., color=color, lw=2)
-    ax.fill_between(prof.index, (prof['mean'] - prof['std'])/energy*100., (prof['mean'] + prof['std'])/energy*100.,
-                    color=color, alpha=0.3, label=title)
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-ax.xaxis.set_minor_locator(MultipleLocator(1))
-ax.yaxis.set_minor_locator(MultipleLocator(1))
-ax.grid(linestyle=':', which='both')
-ax.tick_params(labelsize=24)
-ax.set_xlabel('Layer', fontsize=26)
-ax.set_ylabel('Sampling Fraction (%)', fontsize=26)
-ax.legend(fontsize=26, loc='upper left')
-fig.savefig(os.path.join(args.outdir, 'energy_profile.png'))
-
-
-def calc_chi2(grp, idmin=5):
-    grp2 = grp[grp['id'] >= idmin]
-    mean, std = profe.loc[grp2['id'], ['mean', 'std']].values.T
+def calc_chi2(grp, pr, lmin=5, lmax=12):
+    grp2 = grp[(grp['layer'] >= lmin) & (grp['layer'] <= lmax)]
+    mean, std = pr.loc[grp2['layer'], ['mean', 'std']].values.T*pr['energy'].mean()
     edeps = grp2['edep'].values
     return np.sqrt(np.sum((edeps - mean)**2/std**2)/float(len(edeps)))
 
 
-fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
-chi2 = dfe.groupby('event').apply(calc_chi2)
-ax.hist(chi2, bins=np.linspace(0, 5, 200), weights=[1/float(len(chi2))]*chi2,
-        ec='royalblue', color='royalblue', alpha=0.5, label='electrons')
-chi2 = dfpi.groupby('event').apply(calc_chi2)
-print(chi2[chi2 < 0.7])
-ax.hist(chi2, bins=np.linspace(0, 5, 200), weights=[1/float(len(chi2))]*chi2,
-        ec='indianred', color='indianred', alpha=0.5, label='pions')
-ax.grid(linestyle=':', which='major')
-ax.set_axisbelow(True)
-ax.tick_params(labelsize=24)
-ax.set_xlabel(r'$\chi^2$', fontsize=26)
-ax.set_ylabel('Normalized Counts', fontsize=26)
-# ax.set_yscale('log')
-# ax.set_ylim(1e-3, 1.)
-ax.legend(fontsize=26)
-fig.savefig(os.path.join(args.outdir, 'edep_chi2.png'))
+# execute this script
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='epi_separation')
+    parser.add_argument('efile', type=str, help='path to root file (electrons)')
+    parser.add_argument('pifile', type=str, help='path to root file (pions)')
+    parser.add_argument('--prof', type=str, default='profile.csv', help='path to electron profile')
+    parser.add_argument('--plot-dir', type=str, default='./plots', dest='outdir', help='output directory')
+    parser.add_argument('-b', '--branch-name', type=str, default='EcalBarrelClustersLayers', dest='branch',
+                        help='branch name in the root file (outputLayerCollection from ImagingClusterReco)')
+    parser.add_argument('-m', '--macros', type=str, default='rootlogon.C', dest='macros',
+                        help='root macros to load (accept multiple paths separated by \",\")')
+    args = parser.parse_args()
+
+    os.makedirs(args.outdir, exist_ok=True)
+
+
+    load_root_macros(args.macros)
+    # prepare data
+    dfe = prepare_data(args.efile, branch=args.branch)
+    dfpi = prepare_data(args.pifile, branch=args.branch)
+
+    colors = ['royalblue', 'indianred', 'limegreen']
+    prof = pd.read_csv(args.prof).set_index('layer', drop=True)
+
+    # profile comparison
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
+    for title, color in zip(sorted(prof['type'].unique()), colors):
+        pr = prof[prof['type'] == title]
+        ax.plot(pr.index, pr['mean'], color=color, lw=2)
+        ax.fill_between(pr.index, (pr['mean'] - pr['std']), (pr['mean'] + pr['std']),
+                        color=color, alpha=0.3, label=title)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
+    ax.grid(linestyle=':', which='both')
+    ax.tick_params(labelsize=24)
+    ax.set_xlabel('Layer', fontsize=26)
+    ax.set_ylabel('Energy Deposit Percentage', fontsize=26)
+    ax.legend(fontsize=26, loc='upper left')
+    fig.savefig(os.path.join(args.outdir, 'compare_prof.png'))
+
+
+    # check profile
+    layer_range = (4, 12)
+    pre = prof[prof['type'].str.lower() == 'em']
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=160)
+    chi2 = dfe.groupby(['event', 'cluster']).apply(lambda x: calc_chi2(x, pre, *layer_range))
+    ax.hist(chi2, bins=np.linspace(0, 5, 200), weights=[1/float(len(chi2))]*chi2,
+            ec='royalblue', color='royalblue', alpha=0.5, label='electrons')
+    chi2 = dfpi.groupby(['event', 'cluster']).apply(lambda x: calc_chi2(x, pre, *layer_range))
+    # print(chi2[chi2 < 0.7])
+    ax.hist(chi2, bins=np.linspace(0, 5, 200), weights=[1/float(len(chi2))]*chi2,
+            ec='indianred', color='indianred', alpha=0.5, label='pions')
+    ax.grid(linestyle=':', which='major')
+    ax.set_axisbelow(True)
+    ax.tick_params(labelsize=24)
+    ax.set_xlabel(r'$\chi^2$', fontsize=26)
+    ax.set_ylabel('Normalized Counts', fontsize=26)
+    # ax.set_yscale('log')
+    # ax.set_ylim(1e-3, 1.)
+    ax.legend(title=r'$\chi^2$ for $E_{{dep}}$ in layer {:d} - {:d}'.format(*layer_range), title_fontsize=28, fontsize=26)
+    fig.savefig(os.path.join(args.outdir, 'efrac_chi2.png'))
 
