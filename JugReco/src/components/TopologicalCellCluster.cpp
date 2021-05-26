42867d9e125e4a903988ff32973535471e13d7bb
Chao Peng <cpeng@anl.gov>
2021-05-26 14:20:03 +0000

    Add layer sector info to TopologicalCellCluster

--- JugReco/src/components/TopologicalCellCluster.cpp
+++ JugReco/src/components/TopologicalCellCluster.cpp
@@ -1,12 +1,14 @@
 /*
  *  Topological Cell Clustering Algorithm for Sampling Calorimetry
  *  1. group all the adjacent modules
+ *  2. TODO split local maxima (seems no need for Imaging Calorimeter with extremely fine granularity)
  *
  *  Author: Chao Peng (ANL), 04/06/2021
  *  References: https://arxiv.org/pdf/1603.02934.pdf
  *
  */
 #include <algorithm>
+#include "fmt/format.h"
 
 #include "Gaudi/Property.h"
 #include "GaudiAlg/GaudiAlgorithm.h"
@@ -19,6 +21,7 @@
 #include "DDRec/CellIDPositionConverter.h"
 #include "DDRec/SurfaceManager.h"
 #include "DDRec/Surface.h"
+#include "DD4hep/BitFieldCoder.h"
 
 // FCCSW
 #include "JugBase/DataHandle.h"
@@ -34,14 +37,42 @@ namespace Jug::Reco {
 
   class TopologicalCellCluster : public GaudiAlgorithm {
   public:
-    Gaudi::Property<std::vector<double>> u_groupRanges{this, "groupRanges", {1.0, 1.0, 1.0}};
+    // maximum difference in layer numbers that can be considered as neighbours
+    Gaudi::Property<int> m_adjLayerDiff{this, "adjLayerDiff", 1};
+    // geometry service name
+    Gaudi::Property<std::string> m_geoSvcName{this, "geoServiceName", "GeoSvc"};
+    // name of readout class
+    Gaudi::Property<std::string> m_readout{this, "readoutClass", "EcalBarrelHits"};
+    // name of layer field in readout
+    Gaudi::Property<std::string> m_layerField{this, "layerField", "layer"};
+    // name of sector field in readout
+    Gaudi::Property<std::string> m_sectorField{this, "sectorField", "sector"};
+    // maximum distance of local (x, y) to be considered as neighbors at the same layer
+    Gaudi::Property<std::vector<double>> u_localRanges{this, "localRanges", {1.0*mm, 1.0*mm}};
+    // maximum distance of global (eta, phi) to be considered as neighbors at different layers
+    Gaudi::Property<std::vector<double>> u_adjLayerRanges{this, "adjLayerRanges", {0.01*M_PI, 0.01*M_PI}};
+    // maximum global distance to be considered as neighbors in different sectors
+    Gaudi::Property<double> m_adjSectorDist{this, "adjSectorDist", 1.0*cm};
+    // minimum cluster center energy (to be considered as a seed for cluster)
+    // @TODO not used for now, as one can not simply find a center by edep with extremely fine granularity
+    // may need to project to (eta, phi) with crude pixel size to determine the center, which
+    // can happen in the following reconstruction step
     Gaudi::Property<double> m_minClusterCenterEdep{this, "minClusterCenterEdep", 0.5*MeV};
+    // input hits collection
     DataHandle<eic::CalorimeterHitCollection>
         m_inputHitCollection{"inputHitCollection", Gaudi::DataHandle::Reader, this};
+    // output cluster collection
     DataHandle<eic::ClusterCollection>
         m_outputClusterCollection{"outputClusterCollection", Gaudi::DataHandle::Writer, this};
-    DataHandle<eic::CalorimeterHitCollection>
-        m_splitHitCollection{"splitHitCollection", Gaudi::DataHandle::Writer, this};
+    // output split hits collection
+    // @TODO not implemented, as with extreme fine granularity, there is no need to split hits
+    // DataHandle<eic::CalorimeterHitCollection>
+    //     m_splitHitCollection{"splitHitCollection", Gaudi::DataHandle::Writer, this};
+
+    SmartIF<IGeoSvc> m_geoSvc;
+    // visit readout fields
+    dd4hep::BitFieldCoder *id_dec;
+    size_t sector_idx, layer_idx;
 
     // ill-formed: using GaudiAlgorithm::GaudiAlgorithm;
     TopologicalCellCluster(const std::string& name, ISvcLocator* svcLoc)
@@ -49,7 +80,6 @@ namespace Jug::Reco {
     {
         declareProperty("inputHitCollection",       m_inputHitCollection,       "");
         declareProperty("outputClusterCollection",  m_outputClusterCollection,  "");
-        declareProperty("splitHitCollection",       m_splitHitCollection,       "");
     }
 
     StatusCode initialize() override
@@ -58,13 +88,44 @@ namespace Jug::Reco {
             return StatusCode::FAILURE;
         }
 
-        if (u_groupRanges.size() < 3) {
-            error() << "Need 3-dimensional ranges for clustering, only have " << u_groupRanges.size() << endmsg;
+        // check local clustering range
+        if (u_localRanges.size() < 2) {
+            error() << "Need 2-dimensional ranges for same-layer clustering, only have "
+                    << u_localRanges.size() << endmsg;
             return StatusCode::FAILURE;
         }
-        info() << "Cluster group ranges are"
-               << " (" << u_groupRanges[0] << ", " << u_groupRanges[1] << ", " << u_groupRanges[2] << ")"
+        info() << "Same layer hits group ranges"
+               << " (" << u_localRanges[0]/mm << " mm, " << u_localRanges[1]/mm << " mm)"
                << endmsg;
+
+        // check adjacent layer clustering range
+        if (u_adjLayerRanges.size() < 2) {
+            error() << "Need 2-dimensional ranges for adjacent-layer clustering, only have "
+                    << u_adjLayerRanges.size() << endmsg;
+            return StatusCode::FAILURE;
+        }
+        info() << "Same layer hits group ranges"
+               << " (" << u_adjLayerRanges[0]/M_PI*1000. << " mrad, "
+               << u_adjLayerRanges[1]/M_PI*1000. << " mrad)"
+               << endmsg;
+
+        // check geometry service
+        m_geoSvc = service(m_geoSvcName);
+        if (!m_geoSvc) {
+            error() << "Unable to locate Geometry Service. "
+                    << "Make sure you have GeoSvc and SimSvc in the right order in the configuration." << endmsg;
+            return StatusCode::FAILURE;
+        }
+
+        try {
+            id_dec = m_geoSvc->detector()->readout(m_readout).idSpec().decoder();
+            sector_idx = id_dec->index(m_sectorField);
+            layer_idx = id_dec->index(m_layerField);
+        } catch (...) {
+            error() << "Failed to load ID decoder for " << m_readout << endmsg;
+            return StatusCode::FAILURE;
+        }
+
         return StatusCode::SUCCESS;
     }
 
@@ -74,13 +135,16 @@ namespace Jug::Reco {
 	    const auto &hits = *m_inputHitCollection.get();
         // Create output collections
         auto &clusters = *m_outputClusterCollection.createAndPut();
-        auto &split_hits = *m_splitHitCollection.createAndPut();
 
         // group neighboring hits
         std::vector<bool> visits(hits.size(), false);
         std::vector<std::vector<eic::CalorimeterHit>> groups;
         for (size_t i = 0; i < hits.size(); ++i)
         {
+            debug() << fmt::format("hit {}, layer {}, sector {}", i,
+                                   id_dec->get(hits[i].cellID(), layer_idx),
+                                   id_dec->get(hits[i].cellID(), sector_idx))
+                    << endmsg;
             // already in a group
             if (visits[i]) {
                 continue;
@@ -91,7 +155,6 @@ namespace Jug::Reco {
         }
         debug() << "we have " << groups.size() << " groups of hits" << endmsg;
 
-        // TODO: add splitting
         for (auto &group : groups) {
             auto cl = clusters.create();
             for (auto &hit : group) {
@@ -103,12 +166,45 @@ namespace Jug::Reco {
     }
 
 private:
+    template<typename T> static inline T pow2(const T &x) { return x*x; }
+
     // helper function to group hits
-    inline bool is_neighbor(const eic::ConstCalorimeterHit &h1, const eic::ConstCalorimeterHit &h2) const
+    bool is_neighbor(const eic::ConstCalorimeterHit &h1, const eic::ConstCalorimeterHit &h2) const
     {
-        return (std::abs(h1.x() - h2.x()) <= u_groupRanges[0]) &&
-               (std::abs(h1.y() - h2.y()) <= u_groupRanges[1]) &&
-               (std::abs(h1.z() - h2.z()) <= u_groupRanges[2]);
+        // we will merge different sectors later using global positions
+        int s1 = id_dec->get(h1.cellID(), sector_idx);
+        int s2 = id_dec->get(h2.cellID(), sector_idx);
+        if (s1 != s2) {
+            return std::sqrt(pow2(h1.x() - h2.x()) + pow2(h1.y() - h2.y()) + pow2(h1.z() - h2.z())) <= m_adjSectorDist;
+        }
+
+        int l1 = id_dec->get(h1.cellID(), layer_idx);
+        int l2 = id_dec->get(h2.cellID(), layer_idx);
+
+        // layer check
+        int ldiff = std::abs(l1 - l2);
+        // same layer, check local positions
+        if (!ldiff) {
+            return (std::abs(h1.local_x() - h2.local_x()) <= u_localRanges[0]) &&
+                   (std::abs(h1.local_y() - h2.local_y()) <= u_localRanges[1]);
+        } else if (ldiff <= m_adjLayerDiff) {
+            double eta1, phi1, r1, eta2, phi2, r2;
+            calc_eta_phi_r(h1.x(), h1.y(), h1.z(), eta1, phi1, r1);
+            calc_eta_phi_r(h2.x(), h2.y(), h2.z(), eta2, phi2, r2);
+
+            return (std::abs(eta1 - eta2) <= u_adjLayerRanges[0]) &&
+                   (std::abs(phi1 - phi2) <= u_adjLayerRanges[1]);
+        }
+
+        // not in adjacent layers
+        return false;
+    }
+
+    static void calc_eta_phi_r(double x, double y, double z, double &eta, double &phi, double &r) {
+        r = std::sqrt(x*x + y*y + z*z);
+        phi = std::atan2(y, x);
+        double theta = std::acos(z/r);
+        eta = -std::log(std::tan(theta/2.));
     }
 
     // grouping function with Depth-First Search
