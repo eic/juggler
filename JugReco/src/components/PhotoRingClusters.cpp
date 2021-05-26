d8311db1185aca5e522b69451e5aff2909450cdf
Chao Peng <cpeng@anl.gov>
2021-05-25 20:09:17 -0500

    add fit part for PhotoRingCluster

--- JugReco/src/components/PhotoRingClusters.cpp
+++ JugReco/src/components/PhotoRingClusters.cpp
@@ -40,8 +40,14 @@ public:
         m_inputHitCollection{"inputHitCollection", Gaudi::DataHandle::Reader, this};
     DataHandle<eic::RIChClusterCollection>
         m_outputClusterCollection{"outputClusterCollection", Gaudi::DataHandle::Writer, this};
-    Gaudi::Property<double> m_minNpe{this, "minNpe", 0.0};
-    /// Pointer to the geometry service
+    // @TODO
+    // A more realistic way is to have tracker info as the input to determine how much clusters should be found
+    Gaudi::Property<int> m_nRings{this, "nRings", 1};
+    Gaudi::Property<int> m_nIters{this, "nIters", 1000};
+    Gaudi::Property<double> m_q{this, "q", 2.0};
+    Gaudi::Property<double> m_eps{this, "epsilon", 1e-4};
+    Gaudi::Property<double> m_minNpe{this, "minNpe", 0.5};
+    // Pointer to the geometry service
     SmartIF<IGeoSvc> m_geoSvc;
 
     // ill-formed: using GaudiAlgorithm::GaudiAlgorithm;
@@ -69,16 +75,30 @@ public:
     StatusCode execute() override
     {
         // input collections
-	    const auto &rawhits = *m_inputHitCollection.get();
+        const auto &rawhits = *m_inputHitCollection.get();
         // Create output collections
         auto &clusters = *m_outputClusterCollection.createAndPut();
 
         // algorithm
         auto alg = fkc::KRings();
 
+        // fill data
         MatrixXd data(rawhits.size(), 2);
         for (int i = 0; i < data.rows(); ++i) {
-            data.row(i) << rawhits[i].local_x(), rawhits[i].local_y();
+            if (rawhits[i].npe() > m_minNpe) {
+                data.row(i) << rawhits[i].local_x(), rawhits[i].local_y();
+            }
+        }
+
+        // clustering
+        auto res = alg.Fit(data, m_nRings, m_q, m_eps, m_nIters);
+
+        // local position
+        for (size_t i = 0; i < res.rows(); ++i) {
+            auto cl = clusters.create();
+            cl.x(res(i, 0));
+            cl.y(res(i, 1));
+            cl.radius(res(i, 2));
         }
 
         return StatusCode::SUCCESS;
