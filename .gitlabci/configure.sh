d1fad3f0ee346f2eda5b7485f220a8dd9c97b391
Whitney Armstrong <warmstrong@anl.gov>
2021-06-05 23:46:51 -0500

    Revert "Updated CI to integrate with eic_container"
    This reverts commit a3adfd4398b03e8ee8a3c3ac441cb38ef229699b.
    
    Everything stopped working: https://eicweb.phy.anl.gov/EIC/detectors/athena/-/jobs/107144

---
+++ .gitlabci/configure.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Configure a CI file based on a template file (first and only
+## argument to this script).
+
+## Known variables that will be substituted:
+## @EIC_TAG@             - docker tag used for the EIC container
+## @JUGGLER_TAG@         - output tag for the Juggler version
+## @JUGGLER_BRANCH@      - Juggler git branch for build
+
+TEMPLATE_FILE=$1
+OUTPUT_FILE=${TEMPLATE_FILE%.*}
+JUGGLER_BRANCH="master"
+
+if [ -n "${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" ] ; then
+  JUGGLER_BRANCH=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
+fi
+
+echo "Configuring CI file: ${TEMPLATE_FILE}"
+echo "Output will be written to: ${OUTPUT_FILE}"
+
+TAG=$EIC_VERSION
+
+sed "s/@EIC_TAG@/$EIC_TAG/g" $TEMPLATE_FILE | \
+  sed "s/@JUGGLER_TAG@/$JUGGLER_TAG/g" | \
+  sed "s/@EIC_DATA_MODEL_VERSION@/$EIC_DATA_MODEL_VERSION/g" | \
+  sed "s/@JUGGLER_BRANCH@/$JUGGLER_BRANCH/g" > ${OUTPUT_FILE}
+
+
+echo "Done"
