573d4fc89d53cd47b7a22081c68ea0aa0747fff8
Chao Peng <cpeng@anl.gov>
2021-06-09 14:50:41 -0500

    CalorimeterHitsMerger update: use the position of a reference cell for the merged hits group

--- JugReco/src/components/CalorimeterHitsMerger.cpp
+++ JugReco/src/components/CalorimeterHitsMerger.cpp
@@ -39,14 +39,17 @@ class CalorimeterHitsMerger : public GaudiAlgorithm {
 public:
     Gaudi::Property<std::string>                m_geoSvcName{this, "geoServiceName", "GeoSvc"};
     Gaudi::Property<std::string>                m_readout{this, "readoutClass", "EcalBarrelHits"};
-    Gaudi::Property<std::vector<std::string>>   m_fields{this, "fields", {"layer"}};
+    // field names to generate id mask, the hits will be grouped by masking the field
+    Gaudi::Property<std::vector<std::string>>   u_fields{this, "fields", {"layer"}};
+    // reference field numbers to locate position for each merged hits group
+    Gaudi::Property<std::vector<int>>           u_refs{this, "fieldRefNumbers", {}};
     DataHandle<eic::CalorimeterHitCollection>
         m_inputHitCollection{"inputHitCollection", Gaudi::DataHandle::Reader, this};
     DataHandle<eic::CalorimeterHitCollection>
         m_outputHitCollection{"outputHitCollection", Gaudi::DataHandle::Writer, this};
 
     SmartIF<IGeoSvc> m_geoSvc;
-    uint64_t id_mask;
+    uint64_t id_mask, ref_mask;
 
     // ill-formed: using GaudiAlgorithm::GaudiAlgorithm;
     CalorimeterHitsMerger(const std::string& name, ISvcLocator* svcLoc)
@@ -72,16 +75,22 @@ public:
         try {
             auto id_desc = m_geoSvc->detector()->readout(m_readout).idSpec();
             id_mask = 0;
-            for (auto &f : m_fields) {
-                id_mask |= id_desc.field(f)->mask();
+            std::vector<std::pair<std::string, int>> ref_fields;
+            for (size_t i = 0; i < u_fields.size(); ++i) {
+                id_mask |= id_desc.field(u_fields[i])->mask();
+                // use the same ref number if the length of two vectors do not much
+                // or default number (1) if emepty vector provied
+                int ref = u_refs.empty() ? 1 : (i < u_refs.size() ? u_refs[i] : u_refs.value().back());
+                ref_fields.push_back({u_fields[i], ref});
             }
+            ref_mask = id_desc.encode(ref_fields);
+            // debug() << fmt::format("Referece id mask for the fields {:#064b}", ref_mask) << endmsg;
         } catch (...) {
             error() << "Failed to load ID decoder for " << m_readout << endmsg;
             return StatusCode::FAILURE;
         }
         id_mask = ~id_mask;
-        info() << fmt::format("ID mask for [{:s}] fields in {:s}: {:#064b}",
-                              fmt::join(m_fields, ", "), m_readout, id_mask)
+        info() << fmt::format("ID mask in {:s}: {:#064b}", m_readout, id_mask)
                << endmsg;
         return StatusCode::SUCCESS;
     }
@@ -93,6 +102,10 @@ public:
         // Create output collections
         auto &mhits = *m_outputHitCollection.createAndPut();
 
+        // dd4hep decoders
+        auto poscon = m_geoSvc->cellIDPositionConverter();
+        auto volman = m_geoSvc->detector()->volumeManager();
+
         // sum energies that has the same id
         std::unordered_map<long long, size_t> merge_map;
         for (auto &h : hits) {
@@ -101,7 +114,17 @@ public:
             auto it = merge_map.find(id);
             if (it == merge_map.end()) {
                 merge_map[id] = mhits.size();
-                mhits.push_back(h.clone());
+                auto ahit = h.clone();
+                // use the reference field position
+                int ref_id = id | ref_mask;
+                // global positions
+                auto gpos = poscon->position(ref_id);
+                // local positions
+                auto alignment = volman.lookupDetector(ref_id).nominal();
+                auto pos = alignment.worldToLocal(dd4hep::Position(gpos.x(), gpos.y(), gpos.z()));
+                ahit.position({gpos.x()/dd4hep::mm, gpos.y()/dd4hep::mm, gpos.z()/dd4hep::mm});
+                ahit.local({pos.x()/dd4hep::mm, pos.y()/dd4hep::mm, pos.z()/dd4hep::mm});
+                mhits.push_back(ahit);
                 debug() << mhits[mhits.size() - 1].cellID() << " - " << std::bitset<64>(id) << endmsg;
             } else {
                 mhits[it->second].energy(mhits[it->second].energy() + h.energy());
