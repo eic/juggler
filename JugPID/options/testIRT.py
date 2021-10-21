f3696c1312b9c700c7682c4ee0927f08ff9417e5
christopher dilks <christopher.j.dilks@gmail.com>
2021-10-21 01:02:51 -0400

    modify testIRT

--- JugPID/options/testIRT.py
+++ JugPID/options/testIRT.py
@@ -14,11 +14,13 @@ input_sim_file  = str(os.environ["JUGGLER_SIM_FILE"])
 output_rec_file = str(os.environ["JUGGLER_REC_FILE"])
 n_events = str(os.environ["JUGGLER_N_EVENTS"])
 
-geo_service  = GeoSvc("GeoSvc", detectors=["{}.xml".format(detector_name)])
-podioevent   = EICDataSvc("EventDataSvc", inputs=[input_sim_file], OutputLevel=DEBUG)
+geo_service = GeoSvc("GeoSvc", detectors=["{}.xml".format(detector_name)])
+podioevent = EICDataSvc("EventDataSvc", inputs=[input_sim_file], OutputLevel=DEBUG)
 
 from Configurables import PodioInput
-from Configurables import Jug__PID__TestIRT as TestIRT
+from Configurables import Jug__Digi__PhotoMultiplierDigi as PhotoMultiplierDigi
+from Configurables import Jug__Reco__PhotoMultiplierReco as PhotoMultiplierReco
+from Configurables import Jug__Reco__TestIRT as TestIRT
 
 podioinput = PodioInput(
         "PodioReader",
@@ -27,42 +29,19 @@ podioinput = PodioInput(
         )
 
 irtrec = TestIRT(
-        "irt_rec"#,
-        #inputHitCollection="ERICHHits"
+        "irt_rec",
+        inputHitCollection="ERICHHits",
+        outputPidCollection="ERICHPid"
         )
 
-out = PodioOutput("out", filename="reco_testIRT.root")
+out = PodioOutput("out", filename=output_rec_file)
 out.outputCommands = ["keep *"]
+
 ApplicationMgr(
         TopAlg = [podioinput, irtrec, out],
         EvtSel = 'NONE',
-        EvtMax   = n_events,
+        EvtMax = n_events,
         ExtSvc = [podioevent],
-        OutputLevel=DEBUG
+        OutputLevel = DEBUG
         )
-
-# REFERENCE: from reconstruction benchmark options file `rich_reco.py` #############
-#
-# from Configurables import Jug__Digi__PhotoMultiplierDigi as PhotoMultiplierDigi
-# from Configurables import Jug__Reco__PhotoMultiplierReco as PhotoMultiplierReco
-# from Configurables import Jug__Reco__PhotoRingClusters as PhotoRingClusters
-#
-# qe_data = [(1.0, 0.25), (7.5, 0.25),]
-#
-# pmtdigi = PhotoMultiplierDigi(inputHitCollection="ERICHHits", outputHitCollection="DigiForwardRICHHits",
-#                               quantumEfficiency=[(a*units.eV, b) for a, b in qe_data])
-# pmtreco = PhotoMultiplierReco(inputHitCollection="DigiForwardRICHHits", outputHitCollection="RecoForwardRICHHits")
-# richcluster = PhotoRingClusters(inputHitCollection="RecoForwardRICHHits", #inputTrackCollection="mcparticles",
-#                                 outputClusterCollection="RICHClusters")
-# 
-# out = PodioOutput("out", filename=output_rec_file)
-# out.outputCommands = ["keep *"]
-# 
-# ApplicationMgr(
-#     TopAlg = [podioinput, pmtdigi, pmtreco, richcluster, out],
-#     EvtSel = 'NONE',
-#     EvtMax = n_events,
-#     ExtSvc = [podioevent],
-#     OutputLevel=DEBUG
-#  )
 
