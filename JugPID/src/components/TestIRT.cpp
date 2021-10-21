50625e9ec98b4d605e1c7af7d5953c5a7de1140c
christopher dilks <christopher.j.dilks@gmail.com>
2021-10-21 01:02:51 -0400

    modify testIRT

--- JugPID/src/components/TestIRT.cpp
+++ JugPID/src/components/TestIRT.cpp
@@ -1,29 +1,96 @@
-#include "TestIRT.h"
-#include "GaudiKernel/MsgStream.h"
+#include <algorithm>
 
-namespace Jug::PID {
-  TestIRT::TestIRT(const std::string& name, ISvcLocator* ploc)
-      : GaudiAlgorithm(name, ploc) {}
+#include "Gaudi/Property.h"
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiAlg/Transformer.h"
+#include "GaudiKernel/PhysicalConstants.h"
+#include "GaudiKernel/RndmGenerators.h"
+#include "GaudiKernel/ToolHandle.h"
 
-  StatusCode TestIRT::initialize() {
+#include "DDRec/CellIDPositionConverter.h"
+#include "DDRec/Surface.h"
+#include "DDRec/SurfaceManager.h"
 
-    StatusCode sc = Algorithm::initialize();
-    if (sc.isFailure())
-      return sc;
+#include "JugBase/DataHandle.h"
+#include "JugBase/IGeoSvc.h"
+#include "JugBase/UniqueID.h"
 
-    info() << "TestIRT: Inilializing..." << endmsg;
-    return StatusCode::SUCCESS;
-  }
+// Event Model related classes
+#include "eicd/PMTHitCollection.h"
+#include "eicd/RingImageCollection.h" // TODO: remove when no longer needed
 
-  StatusCode TestIRT::execute() {
-    info() << "TestIRT: executing..." << endmsg;
-    return StatusCode::SUCCESS;
-  }
+using namespace Gaudi::Units;
 
-  StatusCode TestIRT::finalize() {
-    info() << "TestIRT: Finalizing..." << endmsg;
-    return Algorithm::finalize(); // must be executed last
-  }
+namespace Jug::Reco {
+  /** RICH IRT
+   *
+   * \ingroup reco
+   */
+  class TestIRT : public GaudiAlgorithm, AlgorithmIDMixin<> {
+  public:
+
+    // local vars and pointers
+    SmartIF<IGeoSvc> m_geoSvc; // geometry service
+    DataHandle<eic::PMTHitCollection> m_inputHitCollection{
+      "inputHitCollection",
+      Gaudi::DataHandle::Reader,
+      this
+    };
+    DataHandle<eic::RingImageCollection> m_outputPidCollection{ // TODO: change `RingImageCollection`
+      "outputPidCollection",
+      Gaudi::DataHandle::Writer,
+      this
+    };
+
+    // ------------------------------------------------------------
+
+    // constructor
+    TestIRT(const std::string& name, ISvcLocator* svcLoc) 
+      : GaudiAlgorithm(name, svcLoc)
+      , AlgorithmIDMixin(name, info())
+    {
+      declareProperty("inputHitCollection", m_inputHitCollection, "");
+      declareProperty("outputPidCollection", m_outputPidCollection, "");
+    }
+
+
+    StatusCode initialize() override {
+      if (GaudiAlgorithm::initialize().isFailure()) {
+        return StatusCode::FAILURE;
+      }
+      m_geoSvc = service("GeoSvc");
+      if (!m_geoSvc) {
+        error() << "Unable to locate Geometry Service. "
+                << "Make sure you have GeoSvc and SimSvc in the right order in the configuration." << endmsg;
+        return StatusCode::FAILURE;
+      }
+      return StatusCode::SUCCESS;
+    }
+
+
+    StatusCode execute() override
+    {
+      // input collections
+      const auto& rawhits = *m_inputHitCollection.get();
+      // output collections
+      auto& results = *m_outputPidCollection.createAndPut();
+
+      // algorithm
+      //auto alg = IRT CODE //// TODO
+
+      return StatusCode::SUCCESS;
+    }
+
+
+    StatusCode finalize() override {
+      info() << "TestIRT: Finalizing..." << endmsg;
+      return Algorithm::finalize(); // must be executed last
+    }
+
+
+  };
 
   DECLARE_COMPONENT(TestIRT)
-} // namespace Jug::PID
+
+} // namespace Jug::Reco
