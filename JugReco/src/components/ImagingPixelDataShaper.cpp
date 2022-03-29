7ab149e340410d762261e5fdcff39ef2bf470226
Robinpreet Dhillon <rdhill13@student.ubc.ca>
2022-03-29 18:42:29 +0000

    Update JugReco/src/components/ImagingPixelDataShaper.cpp

--- JugReco/src/components/ImagingPixelDataShaper.cpp
+++ JugReco/src/components/ImagingPixelDataShaper.cpp
@@ -65,45 +65,52 @@ public:
 
   StatusCode execute() override {
     // input collections
-    const auto hits = m_inputHits.get();
-    
-    auto coll = hits;
-    int count = 1;
-    for (auto hit : *coll) {
-      cout << count << endl;
-      // cout << hit.getLayer().size_t << endl;
-      count++;
-      // eicd::CalorimeterHit h2{
-      //   hit.getEnergy(),  hit.getPosition(), hit.getLayer(),
-      //   };
+    const auto& hits = *m_inputHits.get();  // 1d
+
+    // group the hits by layer
+    std::vector<std::vector<eicd::CalorimeterHit>> layer_hits;  // create a vector of vector of hits
+    layer_hits.resize(int m_nLayers = 29);  // create 29 layers/rows 
+    for (const auto& h : hits) {
+      auto k = h.getLayer();
+      if ((int)k < m_nLayers) {
+        layer_hits[k].push_back(h);  // take each hit and append it to vector above 
+      }
     }
 
+    // sort by energy. Descending energies for each layer. 
+    for (auto &layer : layer_hits) {
+      std::sort(layer.begin(), layer.end(),
+        [] (const eicd::CalorimeterHit &h1, const eicd::CalorimeterHit &h2) {
+          return h1.getEnergy() > h2.getEnergy();
+        });
+      layer.resize(int topHits = 20);  // get only the 20 most energetic hits for each layer
+    }
+
+    int layers = 29;
+    int nHits = 20;
+    int features = 5;
     
+    float output[layers][nHits][features];
 
-    // int layers = 29;
-    // int nHits = 20;
-    // int features = 5;
-    // float output[layers][nHits][features];
+    for (int i = 0; i < layer_hits[0].size(); i++) {  // 29 layers
+      for (int j = 0; j < layer_hits[1].size; j++) {  // 20 most energetic hits
+          x = layer_hits[i][j].getPosition().x;
+          y = layer_hits[i][j].getPosition().y;
+          z = layer_hits[i][j].getPosition().z;
 
-    // for (int i = 0; i < layers; i++) {  // 29 layers
-    //   for (int j = 0; j < nHits; j++) {  // 20 most energetic hits
-    //     for (int k = 0; k < features; k++){  // 5 features of each hit
-    //       x = hits.getPosition().x;
-    //       y = hits.getPosition().y;
-    //       z = hits.getPosition().z;
+          rc = sqrt(pow(x, 2) + pow(y, 2)); 
+          r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+          theta = acos(z/r);
+          phi = atan2(y, x)
+          eta = -1*log10(tan(theta/2))
 
-    //       rc = sqrt(pow(x, 2) + pow(y, 2));
-    //       r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    //       theta = acos(z/r);
-    //       phi = atan2(hits.getPosition.y, hits.getPosition.x)
-    //       eta = -1*log10(tan(theta/ 2.))
-
-    //       layer_type = floor(hits.getLayer());
-    //       energy = hits.getEnergy();
-    //       output[i][j][k] = {layer_type, energy, rc, eta, phi}
-    //     }
-    //   }
-    // }
+          layer_type = floor(layer_hits[i][j].getLayer());
+          energy = layer_hits[i][j].getEnergy();
+             
+          output[i][j] = {layer_type, energy, rc, eta, phi}
+        }
+      }
+    }
       
 
     return StatusCode::SUCCESS;
