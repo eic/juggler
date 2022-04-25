76e6bd3cce92934ffc7080dd710312fcd5b95430
Wouter Deconinck <wdconinc@gmail.com>
2022-04-24 20:44:39 -0500

    Minor fixes

--- JugBase/src/components/EICDataSvc.h
+++ JugBase/src/components/EICDataSvc.h
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2022 Whitney Armstrong
 
-#ifndef JUGBASE_FCCDATASVC_H
-#define JUGBASE_FCCDATASVC_H
+#ifndef JUGBASE_EICDATASVC_H
+#define JUGBASE_EICDATASVC_H
 
 #include "JugBase/PodioDataSvc.h"
 
@@ -15,4 +15,4 @@ public:
   /// Standard Destructor
   virtual ~EICDataSvc();
 };
-#endif  // JUGBASE_FCCDATASVC_H
+#endif  // JUGBASE_EICDATASVC_H
