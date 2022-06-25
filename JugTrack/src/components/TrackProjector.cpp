af1ac54e8f170dc3d4074631089a58ebc5e99af8
Wouter Deconinck <wdconinc@gmail.com>
2022-06-24 22:31:25 -0500

    Minimally compiling TrackProjector (functionality affected!)

--- JugTrack/src/components/TrackProjector.cpp
+++ JugTrack/src/components/TrackProjector.cpp
@@ -3,8 +3,6 @@
 
 #include <algorithm>
 
-// FIXME needs renaming (TrackProjector) and updating to fix and remove hardcoded numbers
-
 // Gaudi
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "GaudiKernel/ToolHandle.h"
@@ -28,6 +26,7 @@
 #include "eicd/TrackerHitCollection.h"
 #include "eicd/TrackParametersCollection.h"
 #include "eicd/TrajectoryCollection.h"
+#include "eicd/TrackSegmentCollection.h"
 #include "JugTrack/IndexSourceLink.hpp"
 #include "JugTrack/Track.hpp"
 #include "JugTrack/Trajectories.hpp"
@@ -50,17 +49,25 @@ namespace Jug::Reco {
    *
    * \ingroup tracking
    */
-   class TrajectoryFromTrackFit : public GaudiAlgorithm {
-   public:
-    DataHandle<TrajectoriesContainer>     m_inputTrajectories{"inputTrajectories", Gaudi::DataHandle::Reader, this};
-    DataHandle<eicd::TrajectoryCollection> m_outputTrajectory{"outputTrajectoryParameters", Gaudi::DataHandle::Writer, this};
+   class TrackProjector : public GaudiAlgorithm {
+   private:
+    DataHandle<TrajectoriesContainer>        m_inputTrajectories{"inputTrajectories", Gaudi::DataHandle::Reader, this};
+    DataHandle<eicd::TrackSegmentCollection> m_outputTrackSegments{"outputTrackSegments", Gaudi::DataHandle::Writer, this};
 
-   public:
+    Gaudi::Property<unsigned int> m_firstInVolumeID{this, "firstInVolumeID", 0};
+    Gaudi::Property<std::string> m_firstInVolumeName{this, "firstInVolumeName", ""};
+    Gaudi::Property<float> m_firstSmallerThanZ{this, "firstSmallerThanZ", 0};
+    Gaudi::Property<float> m_firstGreaterThanZ{this, "firstGreaterThanZ", 0};
+    Gaudi::Property<float> m_firstGreaterThanR{this, "firstGreaterThanR", -1};
+
+    Acts::GeometryContext m_geoContext;
+
+    public:
     //  ill-formed: using GaudiAlgorithm::GaudiAlgorithm;
-    TrajectoryFromTrackFit(const std::string& name, ISvcLocator* svcLoc)
-        : GaudiAlgorithm(name, svcLoc)) {
+    TrackProjector(const std::string& name, ISvcLocator* svcLoc)
+        : GaudiAlgorithm(name, svcLoc) {
           declareProperty("inputTrajectories", m_inputTrajectories,"");
-          declareProperty("outputTrajectoryParameters", m_outputTrajectory, "ACTS Trajectory Parameters");
+          declareProperty("outputTrackSegments", m_outputTrackSegments, "");
         }
 
     StatusCode initialize() override {
@@ -71,31 +78,31 @@ namespace Jug::Reco {
 
     StatusCode execute() override {
       // input collection
-      const TrajectoriesContainer* trajectories = m_inputTrajectories.get();
+      const auto* const trajectories = m_inputTrajectories.get();
       // create output collections
-      auto traj_pars = m_outputTrajectory.createAndPut();
+      auto* track_segments = m_outputTrackSegments.createAndPut();
 
       if (msgLevel(MSG::DEBUG)) {
         debug() << std::size(*trajectories) << " trajectories " << endmsg;
       }
 
       // Loop over the trajectories
-      for (size_t itraj = 0; itraj < trajectories->size(); ++itraj) {
-        const auto& traj = (*trajectories)[itraj];
-
+      for (const auto& traj : *trajectories) {
         // Get the entry index for the single trajectory
         // The trajectory entry indices and the multiTrajectory
         const auto& mj        = traj.multiTrajectory();
         const auto& trackTips = traj.tips();
-        debug() << "# of elements in trackTips " <<trackTips.size() << endmsg;
+        if (msgLevel(MSG::DEBUG)) {
+          debug() << "# of elements in trackTips " <<trackTips.size() << endmsg;
+        }
 
+        // Skip empty
         if (trackTips.empty()) {
           if (msgLevel(MSG::DEBUG)) {
             debug() << "Empty multiTrajectory." << endmsg;
           }
           continue;
         }
-
         auto& trackTip = trackTips.front();
 
         // Collect the trajectory summary info
@@ -104,34 +111,97 @@ namespace Jug::Reco {
         int  m_nStates       = trajState.nStates;
         int  m_nCalibrated   = 0;
         if (msgLevel(MSG::DEBUG)) {
-    		  debug() << "n measurement in trajectory " << m_nMeasurements << endmsg;
-    		  debug() << "n state in trajectory " << m_nStates << endmsg;
+          debug() << "n measurement in trajectory " << m_nMeasurements << endmsg;
+          debug() << "n state in trajectory " << m_nStates << endmsg;
         }
 
-        // get path length at last silicon layer
-        float pathlength_at_reflayer = -9999.;
+        eicd::MutableTrackSegment track_segment;
+
+        // visit the track points
         mj.visitBackwards(trackTip, [&](auto&& trackstate) {
-          // debug() << trackstate.hasPredicted() << endmsg;
-          // debug() << trackstate.predicted() << endmsg;
-          auto params = trackstate.predicted(); //<< endmsg;
-          auto pathlength = trackstate.pathLength();
+          auto pathLength = trackstate.pathLength();
+
+          // get volume info
           auto geoID = trackstate.referenceSurface().geometryId();
           auto volume = geoID.volume();
           auto layer = geoID.layer();
-          if (trackstate.hasCalibrated())
-          {
+          if (trackstate.hasCalibrated()) {
             m_nCalibrated++;
-          } 
+          }
 
-          if (pathlength_at_reflayer<0 && volume==29 && layer==4 && trackstate.hasCalibrated()) pathlength_at_reflayer = pathlength; // 2nd outter barrel
-          if (pathlength_at_reflayer<0 && pathlength>1700) pathlength_at_reflayer = pathlength; // endcap GEM layer
+          // get track state parameters and their covariances
+          const auto& parameter = trackstate.parameters();
+          const auto& covariance = trackstate.covariance();
+
+          // convert local to global
+          auto global = trackstate.referenceSurface().localToGlobal(
+            m_geoContext,
+            {parameter[Acts::eBoundLoc0], parameter[Acts::eBoundLoc1]},
+            {0, 0, 0}
+          );
+          // global position
+          const eicd::Vector3f position {
+            global.x(),
+            global.y(),
+            global.z()
+          };
+
+          // local position
+          const eicd::Vector2f loc {
+            parameter[Acts::eBoundLoc0],
+            parameter[Acts::eBoundLoc1]
+          };
+          const eicd::Cov2f covLoc {
+            static_cast<float>(covariance(Acts::eBoundLoc0, Acts::eBoundLoc0)),
+            static_cast<float>(covariance(Acts::eBoundLoc1, Acts::eBoundLoc1)),
+            static_cast<float>(covariance(Acts::eBoundLoc0, Acts::eBoundLoc1))
+          };
+          const eicd::Cov3f positionError{0, 0, 0};
+          const eicd::Vector3f momentum = eicd::sphericalToVector(
+            1.0 / std::abs(parameter[Acts::eBoundQOverP]),
+            parameter[Acts::eBoundTheta],
+            parameter[Acts::eBoundPhi]
+          );
+          const eicd::Cov3f covMomentum {
+            static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundTheta)),
+            static_cast<float>(covariance(Acts::eBoundPhi, Acts::eBoundPhi)),
+            static_cast<float>(covariance(Acts::eBoundQOverP, Acts::eBoundQOverP)),
+            static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundPhi)),
+            static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundQOverP)),
+            static_cast<float>(covariance(Acts::eBoundPhi, Acts::eBoundQOverP))
+          };
+          const float time{static_cast<float>(parameter(Acts::eBoundTime))};
+          const float timeError{sqrt(static_cast<float>(covariance(Acts::eBoundTime, Acts::eBoundTime)))};
+          const float theta(parameter[Acts::eBoundTheta]);
+          const float phi(parameter[Acts::eBoundPhi]);
+          const eicd::Cov2f directionError {
+            static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundTheta)),
+            static_cast<float>(covariance(Acts::eBoundPhi, Acts::eBoundPhi)),
+            static_cast<float>(covariance(Acts::eBoundTheta, Acts::eBoundPhi))
+          };
+          const float pathLengthError = 0;
+
+          // Store track point
+          track_segment.addToPoints({
+            position,
+            positionError,
+            momentum,
+            covMomentum,
+            time,
+            timeError,
+            theta,
+            phi,
+            directionError,
+            static_cast<float>(pathLength),
+            pathLengthError
+          });
 
           if (msgLevel(MSG::DEBUG)) {
             debug() << "******************************" << endmsg;
             debug() << "predicted variables: \n" << trackstate.predicted() << endmsg;
             debug() << "geoID = " << geoID << endmsg;
             debug() << "volume = " << volume << ", layer = " << layer << endmsg;
-            debug() << "pathlength = " << pathlength << endmsg;
+            debug() << "pathlength = " << pathLength << endmsg;
             debug() << "hasCalibrated = " << trackstate.hasCalibrated() << endmsg;
             debug() << "******************************" << endmsg;
           }
@@ -141,39 +211,8 @@ namespace Jug::Reco {
           debug() << "n calibrated state in trajectory " << m_nCalibrated << endmsg;
         }
 
-        // Get the fitted track parameter
-        bool m_hasFittedParams = false;
-        int ID = 0;
-        float charge = -9999.;
-        float tof = -9999.;
-        if (traj.hasTrackParameters(trackTip)) {
-          m_hasFittedParams      = true;
-          const auto& boundParam = traj.trackParameters(trackTip);
-          const auto& parameter  = boundParam.parameters();
-          const auto& covariance = *boundParam.covariance();
-          charge = boundParam.charge();
-
-          double p = 1.0 / parameter[Acts::eBoundQOverP];
-          double p_phi = parameter[Acts::eBoundPhi];
-          double p_theta = parameter[Acts::eBoundTheta];
-          eicd::VectorXYZT mom(p*sin(p_theta)*cos(p_phi), p*sin(p_theta)*sin(p_phi), p*cos(p_theta), 0);
-
-          if (msgLevel(MSG::DEBUG)) {
-            debug() << " chi2 = " << trajState.chi2Sum << endmsg;
-            debug() << " pathlength at reference layer = " << pathlength_at_reflayer << "mm" << endmsg;
-          }
-
-          eicd::Trajectory traj_par{
-            {ID++, algorithmID()},
-            {0, 0}, // proto track ID
-            {0, 0}, // track param ID
-            mom, // vectex 4-vector
-            pathlength_at_reflayer,
-            charge,
-            tof};
-
-          traj_pars->push_back(traj_par);
-        }
+        // Add to output collection
+        track_segments->push_back(track_segment);
       }
 
       return StatusCode::SUCCESS;
@@ -181,6 +220,6 @@ namespace Jug::Reco {
 
   };
   // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-  DECLARE_COMPONENT(TrajectoryFromTrackFit)
+  DECLARE_COMPONENT(TrackProjector)
 
 } // namespace Jug::Reco
