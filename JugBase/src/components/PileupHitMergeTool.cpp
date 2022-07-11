3dfe1feb39bdf75bc17d7cc30a531d9d1495c600
Wouter Deconinck <wdconinc@gmail.com>
2022-07-11 19:25:13 +0200

    Remove DigiTrackHit and Particles mergers; fix PileupHitMergeTool compilation

--- JugBase/src/components/PileupHitMergeTool.cpp
+++ JugBase/src/components/PileupHitMergeTool.cpp
@@ -2,44 +2,36 @@
 
 #include "podio/EventStore.h"
 
-#include "edm4hep/CaloHitCollection.h"
-#include "edm4hep/PositionedCaloHitCollection.h"
-#include "edm4hep/PositionedTrackHitCollection.h"
-#include "edm4hep/TrackHitCollection.h"
+#include "edm4hep/SimCalorimeterHitCollection.h"
+#include "edm4hep/SimTrackerHitCollection.h"
 
-typedef PileupHitMergeTool<edm4hep::CaloHitCollection, edm4hep::PositionedCaloHitCollection> PileupCaloHitMergeTool;
-typedef PileupHitMergeTool<edm4hep::TrackHitCollection, edm4hep::PositionedTrackHitCollection> PileupTrackHitMergeTool;
+typedef PileupHitMergeTool<edm4hep::SimCalorimeterHitCollection> PileupCaloHitMergeTool;
+typedef PileupHitMergeTool<edm4hep::SimTrackerHitCollection> PileupTrackHitMergeTool;
 DECLARE_COMPONENT(PileupTrackHitMergeTool)
-DECLARE_COMPONENT_WITH_ID(PileupTrackHitMergeTool, "PileupTrackHitMergeTool")
 DECLARE_COMPONENT(PileupCaloHitMergeTool)
-DECLARE_COMPONENT_WITH_ID(PileupCaloHitMergeTool, "PileupCaloHitMergeTool")
 
-template <class Hits, class PositionedHits>
-PileupHitMergeTool<Hits, PositionedHits>::PileupHitMergeTool(const std::string& aType, const std::string& aName,
-                                                             const IInterface* aParent)
+template <class Hits>
+PileupHitMergeTool<Hits>::PileupHitMergeTool(const std::string& aType, const std::string& aName,
+                                             const IInterface* aParent)
     : GaudiTool(aType, aName, aParent) {
-  declareInterface<IEDMMergeTool>(this);
   declareProperty("signalHits", m_hitsSignal);
-  declareProperty("signalPositionedHits", m_posHitsSignal);
   declareProperty("mergedHits", m_hitsMerged);
-  declareProperty("mergedPositionedHits", m_posHitsMerged);
 }
 
-template <class Hits, class PositionedHits>
-StatusCode PileupHitMergeTool<Hits, PositionedHits>::initialize() {
+template <class Hits>
+StatusCode PileupHitMergeTool<Hits>::initialize() {
   return StatusCode::SUCCESS;
 }
 
-template <class Hits, class PositionedHits>
-StatusCode PileupHitMergeTool<Hits, PositionedHits>::finalize() {
+template <class Hits>
+StatusCode PileupHitMergeTool<Hits>::finalize() {
   return StatusCode::SUCCESS;
 }
 
-template <class Hits, class PositionedHits>
-StatusCode PileupHitMergeTool<Hits, PositionedHits>::readPileupCollection(podio::EventStore& store) {
+template <class Hits>
+StatusCode PileupHitMergeTool<Hits>::readPileupCollection(podio::EventStore& store) {
   // local pointers, to be filled by the event store
   const Hits* hitCollection;
-  const PositionedHits* posHitCollection;
 
   // get collection address and store it in container
   bool hitCollectionPresent = store.get(m_pileupHitsBranchName, hitCollection);
@@ -50,37 +42,25 @@ StatusCode PileupHitMergeTool<Hits, PositionedHits>::readPileupCollection(podio:
     return StatusCode::FAILURE;
   }
 
-  /// as above, for the positioned collection
-  bool posHitCollectionPresent = store.get(m_pileupPosHitsBranchName, posHitCollection);
-  if (posHitCollectionPresent) {
-    m_posHitCollections.push_back(posHitCollection);
-  } else {
-    warning() << "No collection could be read from branch " << m_pileupPosHitsBranchName << endmsg;
-    return StatusCode::FAILURE;
-  }
-
   return StatusCode::SUCCESS;
 }
 
-template <class Hits, class PositionedHits>
-StatusCode PileupHitMergeTool<Hits, PositionedHits>::readSignal() {
+template <class Hits>
+StatusCode PileupHitMergeTool<Hits>::readSignal() {
   // get collection from event sture
   auto collHitsSig = m_hitsSignal.get();
-  auto collPosHitsSig = m_posHitsSignal.get();
 
   // store them in internal container
   m_hitCollections.push_back(collHitsSig);
-  m_posHitCollections.push_back(collPosHitsSig);
 
   return StatusCode::SUCCESS;
 }
 
-template <class Hits, class PositionedHits>
-StatusCode PileupHitMergeTool<Hits, PositionedHits>::mergeCollections() {
+template <class Hits>
+StatusCode PileupHitMergeTool<Hits>::mergeCollections() {
 
   // ownership given to data service at end of execute
   Hits* collHitsMerged = new Hits();
-  PositionedHits* collPosHitsMerged = new PositionedHits();
 
   unsigned int collectionCounter = 0;
   for (auto hitColl : m_hitCollections) {
@@ -92,6 +72,7 @@ StatusCode PileupHitMergeTool<Hits, PositionedHits>::mergeCollections() {
       // i.e. for the signal event, 'bits' is just the trackID taken from geant
       // for the n-th pileup event, 'bits' is the trackID + n * offset
       // offset needs to be big enough to ensure uniqueness of trackID
+/*
       if (elem.bits() > m_trackIDCollectionOffset) {
         error() << "Event contains too many tracks to guarantee a unique trackID";
         error() << " The offset width or trackID field size needs to be adjusted!" << endmsg;
@@ -99,21 +80,13 @@ StatusCode PileupHitMergeTool<Hits, PositionedHits>::mergeCollections() {
       }
 
       clon.bits(clon.bits() + collectionCounter * m_trackIDCollectionOffset);
+*/
       collHitsMerged->push_back(clon);
     }
     ++collectionCounter;
   }
-  for (auto posHitColl : m_posHitCollections) {
-    // copy positioned hits
-    for (const auto elem : *posHitColl) {
-      collPosHitsMerged->push_back(elem.clone());
-    }
-  }
 
   m_hitsMerged.put(collHitsMerged);
-  m_posHitsMerged.put(collPosHitsMerged);
-
   m_hitCollections.clear();
-  m_posHitCollections.clear();
   return StatusCode::SUCCESS;
 }
