1fbb346e964c7fe4d3ecd3d77c479a4fe68a9c46
Sylvester Joosten <sylvester.joosten@gmail.com>
2022-09-13 16:24:04 -0500

    Add core algorithm headers

---
+++ external/algorithms/core/include/algorithms/logger.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <array>
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <mutex>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include <algorithms/error.h>
+#include <algorithms/service.h>
+#include <fmt/format.h>
+
+#define endmsg std::flush
+
+// Simple thread-safe logger with optional overrides by the calling framework
+namespace algorithms {
+
+enum class LogLevel : unsigned { kJunk = 0, kDebug = 1, kInfo = 2, kWarning = 3, kError = 4 };
+constexpr std::string_view logLevelName(LogLevel level) {
+  // Compiler can warn if not all of the enum is covered
+  switch (level) {
+  // case LogLevel::kJunk:
+  //   return "JUNK";
+  case LogLevel::kDebug:
+    return "DEBUG";
+  case LogLevel::kInfo:
+    return "INFO";
+  case LogLevel::kWarning:
+    return "WARNING";
+  case LogLevel::kError:
+    return "ERROR";
+  }
+  // Default return to make gcc happy, will never happen
+  return "UNKNOWN";
+}
+
+// Note: the log action is responsible for dealing with concurrent calls
+//       the default LogAction is a thread-safe example
+class LogSvc : public Service<LogSvc> {
+public:
+  using LogAction = std::function<void(LogLevel, std::string_view, std::string_view)>;
+  void defaultLevel(const LogLevel l) { m_level.set(l); }
+  LogLevel defaultLevel() const { return m_level; }
+  void action(LogAction a) { m_action = a; }
+  void report(const LogLevel l, std::string_view caller, std::string_view msg) const {
+    m_action(l, caller, msg);
+  }
+
+private:
+  Property<LogLevel> m_level{this, "defaultLevel", LogLevel::kInfo};
+  LogAction m_action = [](const LogLevel l, std::string_view caller, std::string_view msg) {
+    static std::mutex m;
+    std::lock_guard<std::mutex> lock(m);
+    fmt::print("{} [{}] {}\n", logLevelName(l), caller, msg);
+  };
+  ALGORITHMS_DEFINE_SERVICE(LogSvc)
+};
+
+namespace detail {
+  // Output buffer that calls our global logger's report() function
+  class LoggerBuffer : public std::stringbuf {
+  public:
+    LoggerBuffer(const LogLevel l, std::string_view caller)
+        : m_mylevel{l}, m_caller{caller}, m_logger{algorithms::LogSvc::instance()} {}
+    virtual int sync() {
+      // report should deal with concurrency (the minimal version does)
+      m_logger.report(m_mylevel, m_caller, this->str());
+      this->str("");
+      return 0;
+    }
+
+  private:
+    // The output buffer knows the log level of its associated logger
+    // (eg. is this the debug logger?)
+    LogLevel m_mylevel;
+    const std::string m_caller;
+    const LogSvc& m_logger;
+  };
+  // thread-safe output stream for the logger
+  class LoggerStream {
+  public:
+    LoggerStream(std::string_view caller, const LogLevel level,
+                 const LogLevel threshold = LogSvc::instance().defaultLevel())
+        : m_buffer{level, caller}, m_os{&m_buffer}, m_level{level}, m_threshold{threshold} {}
+    LoggerStream()                    = delete;
+    LoggerStream(const LoggerStream&) = delete;
+
+    template <class Arg> LoggerStream& operator<<(Arg&& streamable) {
+      if (m_level >= m_threshold) {
+        std::lock_guard<std::mutex> lock{m_mutex};
+        m_os << std::forward<Arg>(streamable);
+        return *this;
+      }
+      return *this;
+    }
+    // To support input manipulators such as std::endl
+    // Note: would be better with Concepts
+    using IOManipType1 = std::ostream&(std::ostream&); // this capturs std::endl;
+    using IOManipType2 = std::ios_base&(std::ios_base&);
+    LoggerStream& operator<<(IOManipType1* f) {
+      if (m_level >= m_threshold) {
+        std::lock_guard<std::mutex> lock{m_mutex};
+        f(m_os);
+        return *this;
+      }
+      return *this;
+    }
+    LoggerStream& operator<<(IOManipType2* f) {
+      if (m_level >= m_threshold) {
+        std::lock_guard<std::mutex> lock{m_mutex};
+        f(m_os);
+        return *this;
+      }
+      return *this;
+    }
+    LogLevel threshold() const { return m_threshold; }
+    void threshold(const LogLevel th) { m_threshold = th; }
+
+  private:
+    std::mutex m_mutex;
+    LoggerBuffer m_buffer;
+    std::ostream m_os;
+    const LogLevel m_level;
+    LogLevel m_threshold;
+  };
+} // namespace detail
+
+// Mixin meant to add utility logger functions to algorithms/services/etc
+class LoggerMixin {
+public:
+  LoggerMixin(std::string_view caller, const LogLevel threshold = LogSvc::instance().defaultLevel())
+      : m_caller{caller} {
+    level(threshold);
+  }
+
+public:
+  // Not done through Properties, as that is the responsible of the base Algo or Service
+  void level(const LogLevel threshold) {
+    m_level = threshold;
+    m_error.threshold(m_level);
+    m_warning.threshold(m_level);
+    m_info.threshold(m_level);
+    m_debug.threshold(m_level);
+    m_junk.threshold(m_level);
+  }
+  LogLevel level() const { return m_level; }
+
+protected:
+  detail::LoggerStream& error() const { return m_error; }
+  detail::LoggerStream& warning() const { return m_warning; }
+  detail::LoggerStream& info() const { return m_info; }
+  detail::LoggerStream& debug() const { return m_debug; }
+  detail::LoggerStream& junk() const { return m_junk; }
+
+  // LoggerMixin also provides nice error raising
+  // ErrorTypes needs to derive from Error, and needs to have a constructor that takes two
+  // strings --> TODO add C++20 Concept version
+  template <class ErrorType = Error> void raise(std::string_view msg) const {
+    error() << msg << endmsg;
+    throw ErrorType(msg);
+  }
+
+private:
+  const std::string m_caller;
+  LogLevel m_level;
+  mutable detail::LoggerStream m_error{m_caller, LogLevel::kError};
+  mutable detail::LoggerStream m_warning{m_caller, LogLevel::kWarning};
+  mutable detail::LoggerStream m_info{m_caller, LogLevel::kInfo};
+  mutable detail::LoggerStream m_debug{m_caller, LogLevel::kDebug};
+  mutable detail::LoggerStream m_junk{m_caller, LogLevel::kJunk};
+};
+
+} // namespace algorithms
+
+//#define endmsg std::flush
---
+++ external/algorithms/core/include/algorithms/property.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include <any>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <fmt/core.h>
+
+#include <algorithms/error.h>
+
+namespace algorithms {
+
+class PropertyError : public Error {
+public:
+  PropertyError(std::string_view msg) : Error{msg, "algorithms::PropertyError"} {}
+};
+
+// Configuration/property handling
+class Configurable {
+public:
+  class PropertyBase;
+  using PropertyMap = std::map<std::string_view, PropertyBase&>;
+
+  template <typename T, typename U> void setProperty(std::string_view name, U&& value) {
+    m_props.at(name).set(T(std::forward<U>(value)));
+  }
+  template <typename T> T getProperty(std::string_view name) const {
+    return std::any_cast<T>(m_props.at(name).get());
+  }
+  const PropertyMap& getProperties() const { return m_props; }
+  bool hasProperty(std::string_view name) const {
+    return m_props.count(name) && m_props.at(name).hasValue();
+  }
+
+private:
+  void registerProperty(PropertyBase& prop) {
+    if (m_props.count(prop.name())) {
+      throw PropertyError(fmt::format("Duplicate property name: {}", prop.name()));
+    }
+    m_props.emplace(prop.name(), prop);
+  }
+
+  PropertyMap m_props;
+
+public:
+  class PropertyBase {
+  public:
+    PropertyBase(std::string_view name) : m_name{name} {}
+    virtual void set(std::any v) = 0;
+    virtual std::any get() const = 0;
+    bool hasValue() const { return m_has_value; }
+    std::string_view name() const { return m_name; }
+
+  protected:
+    const std::string m_name;
+    bool m_has_value = false;
+  };
+
+  // A property type that auto-registers itself with the property handler
+  // Essentially a simplified and const-like version of Gaudi::Property
+  template <class T> class Property : public PropertyBase {
+  public:
+    using ValueType = T;
+
+    Property(Configurable* owner, std::string_view name) : PropertyBase{name} {
+      if (owner) {
+        owner->registerProperty(*this);
+      } else {
+        throw PropertyError(
+            fmt::format("Attempting to create Property '{}' without valid owner", name));
+      }
+    }
+    Property(Configurable* owner, std::string_view name, const ValueType& v)
+        : Property(owner, name) {
+      set(v);
+    }
+
+    Property()                      = delete;
+    Property(const Property&)       = delete;
+    void operator=(const Property&) = delete;
+
+    // Only settable by explicitly calling the ::set() member functio n
+    // as we want the Property to mostly act as if it is constant
+    virtual void set(std::any v) {
+      m_value     = std::any_cast<T>(v);
+      m_has_value = true;
+    }
+    // virtual getter for use from PropertyBase - use ::value() instead for a quick
+    // version that does not go through std::any
+    // Get const reference to the value
+    virtual std::any get() const { return m_value; }
+
+    // Direct access to the value. Use this one whenever possible (or go through the
+    // automatic casting)
+    const ValueType& value() const { return m_value; }
+
+    // automatically cast to T
+    operator T() const { return m_value; }
+
+    // act as if this is a const T
+    template <typename U> bool operator==(const U& rhs) const { return m_value == rhs; }
+    template <typename U> bool operator!=(const U& rhs) const { return m_value != rhs; }
+    template <typename U> bool operator>(const U& rhs) const { return m_value > rhs; }
+    template <typename U> bool operator>=(const U& rhs) const { return m_value >= rhs; }
+    template <typename U> bool operator<(const U& rhs) const { return m_value < rhs; }
+    template <typename U> bool operator<=(const U& rhs) const { return m_value <= rhs; }
+    template <typename U> decltype(auto) operator+(const U& rhs) const { return m_value + rhs; }
+    template <typename U> decltype(auto) operator-(const U& rhs) const { return m_value - rhs; }
+    template <typename U> decltype(auto) operator*(const U& rhs) const { return m_value * rhs; }
+    template <typename U> decltype(auto) operator/(const U& rhs) const { return m_value / rhs; }
+
+    // stl collection helpers if needed
+    // forced to be templated so we only declare them when used
+    template <class U = const ValueType> decltype(auto) size() const { return value().size(); }
+    template <class U = const ValueType> decltype(auto) length() const { return value().length(); }
+    template <class U = const ValueType> decltype(auto) empty() const { return value().empty(); }
+    template <class U = ValueType> decltype(auto) clear() { value().clear(); }
+    template <class U = const ValueType> decltype(auto) begin() const { return value().begin(); }
+    template <class U = const ValueType> decltype(auto) end() const { return value().end(); }
+    template <class U = ValueType> decltype(auto) begin() { return value().begin(); }
+    template <class U = ValueType> decltype(auto) end() { return value().end(); }
+    template <class Arg> decltype(auto) operator[](const Arg& arg) const { return value()[arg]; }
+    template <class Arg> decltype(auto) operator[](const Arg& arg) { return value()[arg]; }
+    template <class U = const ValueType>
+    decltype(auto) find(const typename U::key_type& key) const {
+      return value().find(key);
+    }
+    template <class U = ValueType> decltype(auto) find(const typename U::key_type& key) {
+      return value().find(key);
+    }
+    template <class Arg> decltype(auto) erase(const Arg& arg) { return value().erase(arg); }
+
+    // In case our property has operator (), delegate the operator()
+    template <class... Args>
+    decltype(std::declval<ValueType>()(std::declval<Args&&>()...))
+    operator()(Args&&... args) const {
+      return m_value()(std::forward<Args>(args)...);
+    }
+
+  private:
+    T m_value;
+  };
+};
+
+// Property mixin, provides all the configuration functionality for
+// our algorithms and services
+// Currently an alias to Configurable
+using PropertyMixin = Configurable;
+
+} // namespace algorithms
+
+// operator== overload not needed in C++20 as it will call the member version
+#if __cpp_impl_three_way_comparison < 201711
+template <class T, class U>
+bool operator==(const U& rhs, const algorithms::Configurable::Property<T>& p) {
+  return p == rhs;
+}
+#endif
+template <class T>
+std::ostream& operator<<(std::ostream& os, const algorithms::Configurable::Property<T>& p) {
+  return os << p.value();
+}
+// others needed??? TODO
