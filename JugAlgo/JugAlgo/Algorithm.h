7248ac9e7b7174676b306465acf2a5419504603e
Sylvester Joosten <sjoosten@anl.gov>
2022-09-19 15:16:11 +0000

    +depends JugBase

--- JugAlgo/JugAlgo/Algorithm.h
+++ JugAlgo/JugAlgo/Algorithm.h
@@ -3,9 +3,27 @@
 
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "JugAlgo/IAlgoServiceSvc.h"
+#include "JugBase/DataHandle.h"
 #include "algorithms/algorithm.h"
 
+#include <algorithms/type_traits.h>
+#include <string>
+#include <type_traits>
+
 namespace Jug::Algo {
+
+namespace detail {
+  template <class T, class Enable = void> struct handle_type { using type = std::unique_ptr<DataHandle<T>>; };
+  template <class Vector> struct handle_type<Vector, std::enable_if_t<algorithms::is_vector_v<Vector>>> {
+    using type = Gaudi::Property<std::string>;
+  };
+  template <class Optional> struct handle_type<Optional, std::enable_if_t<algorithms::is_optional_v<Optional>>> {
+    using type = Gaudi::Property<std::string>;
+  };
+  template <class T> using handle_type_t = typename handle_type<T>::type;
+  template <class... T> struct DataHandleTuple : std::tuple<handle_type_t<T>...> {};
+
+} // namespace detail
 
 template <class AlgoImpl> class Algorithm : public GaudiAlgorithm {
 public:
@@ -49,6 +67,11 @@ protected:
   bool hasAlgoProp(std::string_view name) const { return m_algo.hasProperty(name); }
 
 private:
+  InputType getInput() const {}
+
+  // template <class Function, class RefTuple, std::size_t... I>
+  // RefTuple initialize
+
   AlgoType m_algo;
 };
 
