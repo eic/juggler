b237ca0cccbdcdee460a2b5d34a2333228c357e0
Sylvester Joosten <sjoosten@anl.gov>
2022-09-23 16:07:13 +0000

    Ensure log level always initialized

--- JugAlgo/src/components/AlgoServiceSvc.cpp
+++ JugAlgo/src/components/AlgoServiceSvc.cpp
@@ -37,37 +37,41 @@ StatusCode AlgoServiceSvc::initialize() {
 
   auto& serviceSvc = algorithms::ServiceSvc::instance();
   info() << "ServiceSvc declared " << serviceSvc.services().size() << " services" << endmsg;
-  // loop over all services and handle each properly
+  // Always initialize the LogSvc first to ensure proper logging for the others
+  {
+    auto& logger = algorithms::LogSvc::instance();
+    const algorithms::LogLevel level{
+        static_cast<algorithms::LogLevel>(msgLevel() > 0 ? msgLevel() - 1 : 0)};
+    info() << "Setting up algorithms::LogSvc with default level " << algorithms::logLevelName(level)
+           << endmsg;
+    logger.defaultLevel(level);
+    logger.action(
+        [this](const algorithms::LogLevel l, std::string_view caller, std::string_view msg) {
+          const std::string text = fmt::format("[{}] {}", caller, msg);
+          if (l == algorithms::LogLevel::kCritical) {
+            this->fatal() << text << endmsg;
+          } else if (l == algorithms::LogLevel::kError) {
+            this->error() << text << endmsg;
+          } else if (l == algorithms::LogLevel::kWarning) {
+            this->warning() << text << endmsg;
+          } else if (l == algorithms::LogLevel::kInfo) {
+            this->info() << text << endmsg;
+          } else if (l == algorithms::LogLevel::kDebug) {
+            this->debug() << text << endmsg;
+          } else if (l == algorithms::LogLevel::kTrace) {
+            this->verbose() << text << endmsg;
+          }
+        });
+    // set own log level to verbose so we actually display everything that is requested
+    // (this overrides what was initally set through the OutputLevel property)
+    updateMsgStreamOutputLevel(MSG::VERBOSE);
+  }
+  // loop over all remaining services and handle each properly
   // Note: this code is kind of dangerous, as getting the types wrong will lead to
   // undefined runtime behavior.
   for (auto [name, svc] : serviceSvc.services()) {
     if (name == algorithms::LogSvc::kName) {
-      auto* logger = static_cast<algorithms::LogSvc*>(svc);
-      const algorithms::LogLevel level{
-          static_cast<algorithms::LogLevel>(msgLevel() > 0 ? msgLevel() - 1 : 0)};
-      info() << "Setting up algorithms::LogSvc with default level "
-             << algorithms::logLevelName(level) << endmsg;
-      logger->defaultLevel(level);
-      logger->action(
-          [this](const algorithms::LogLevel l, std::string_view caller, std::string_view msg) {
-            const std::string text = fmt::format("[{}] {}", caller, msg);
-            if (l == algorithms::LogLevel::kCritical) {
-              this->fatal() << text << endmsg;
-            } else if (l == algorithms::LogLevel::kError) {
-              this->error() << text << endmsg;
-            } else if (l == algorithms::LogLevel::kWarning) {
-              this->warning() << text << endmsg;
-            } else if (l == algorithms::LogLevel::kInfo) {
-              this->info() << text << endmsg;
-            } else if (l == algorithms::LogLevel::kDebug) {
-              this->debug() << text << endmsg;
-            } else if (l == algorithms::LogLevel::kTrace) {
-              this->verbose() << text << endmsg;
-            }
-          });
-      // set own log level to verbose so we actually display everything that is requested
-      // (this overrides what was initally set through the OutputLevel property)
-      updateMsgStreamOutputLevel(MSG::VERBOSE);
+      ; // Logsvc already initialized, do nothing
     } else if (name == algorithms::GeoSvc::kName) {
       // Setup geometry service
       m_geoSvc = service("GeoSvc");
@@ -79,6 +83,10 @@ StatusCode AlgoServiceSvc::initialize() {
       info() << "Setting up algorithms::GeoSvc" << endmsg;
       auto* geo = static_cast<algorithms::GeoSvc*>(svc);
       geo->init(m_geoSvc->detector());
+    } else {
+      fatal() << "Unknown service encountered, please implement the necessary framework hooks"
+              << endmsg;
+      return StatusCode::FAILURE;
     }
   }
 
