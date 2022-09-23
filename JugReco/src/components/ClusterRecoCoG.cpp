d3e11dc28d8c21375f096c75cdd8b1538badc243
Sylvester Joosten <sjoosten@anl.gov>
2022-09-23 15:46:54 +0000

    remove debug code

--- JugReco/src/components/ClusterRecoCoG.cpp
+++ JugReco/src/components/ClusterRecoCoG.cpp
@@ -29,8 +29,6 @@ public:
     setAlgoProp("logWeightBase", m_logWeightBase.value());
     setAlgoProp("energyWeight", m_energyWeight.value());
     setAlgoProp("moduleDimZName", m_moduleDimZName.value());
-    warning() << "DBG DBG type info for etabounds: " << typeid(m_enableEtaBounds.value()).name()
-              << endmsg;
     setAlgoProp("enableEtaBounds", m_enableEtaBounds.value());
     return StatusCode::SUCCESS;
   }
