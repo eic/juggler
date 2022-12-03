9ffb5e7f0292bd12a625d0d2664036968732179a
Wouter Deconinck <wdconinc@gmail.com>
2022-12-03 16:08:15 -0600

    use local_det in const process; clang-format

--- external/algorithms/calorimetry/src/CalorimeterHitReco.cpp
+++ external/algorithms/calorimetry/src/CalorimeterHitReco.cpp
@@ -37,7 +37,8 @@ void CalorimeterHitReco::init() {
     id_dec = id_spec.decoder();
     if (!m_sectorField.value().empty()) {
       sector_idx = id_dec->index(m_sectorField);
-      info() << "Find sector field " << m_sectorField.value() << ", index = " << sector_idx << endmsg;
+      info() << "Find sector field " << m_sectorField.value() << ", index = " << sector_idx
+             << endmsg;
     }
     if (!m_layerField.value().empty()) {
       layer_idx = id_dec->index(m_layerField);
@@ -54,7 +55,8 @@ void CalorimeterHitReco::init() {
       local = m_geo.detector()->detector(m_localDetElement.value());
       info() << "Local coordinate system from DetElement " << m_localDetElement.value() << endmsg;
     } catch (...) {
-      error() << "Failed to locate local coordinate system from DetElement " << m_localDetElement.value() << endmsg;
+      error() << "Failed to locate local coordinate system from DetElement "
+              << m_localDetElement.value() << endmsg;
       return;
     }
     // or get from fields
@@ -68,8 +70,9 @@ void CalorimeterHitReco::init() {
     if (fields.empty()) {
       local_mask = ~0;
     }
-    info() << fmt::format("Local DetElement mask {:#064b} from fields [{}]", local_mask, fmt::join(fields, ", "))
-            << endmsg;
+    info() << fmt::format("Local DetElement mask {:#064b} from fields [{}]", local_mask,
+                          fmt::join(fields, ", "))
+           << endmsg;
   }
 
   return;
@@ -85,8 +88,8 @@ void CalorimeterHitReco::process(const CalorimeterHitReco::Input& input,
   // energy time reconstruction
   for (const auto& rh : *rawhits) {
 
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic error "-Wsign-conversion"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic error "-Wsign-conversion"
 
     // did not pass the zero-suppression threshold
     if (rh.getAmplitude() < m_pedMeanADC + thresholdADC) {
@@ -94,60 +97,62 @@ void CalorimeterHitReco::process(const CalorimeterHitReco::Input& input,
     }
 
     // convert ADC -> energy
-    const float energy =
-      (((signed)rh.getAmplitude() - (signed)m_pedMeanADC)) / static_cast<float>(m_capADC.value()) * dyRangeADC / m_sampFrac;
-    const float time  = rh.getTimeStamp() / stepTDC;
+    const float energy = (((signed)rh.getAmplitude() - (signed)m_pedMeanADC)) /
+                         static_cast<float>(m_capADC.value()) * dyRangeADC / m_sampFrac;
+    const float time = rh.getTimeStamp() / stepTDC;
 
-    #pragma GCC diagnostic pop
+#pragma GCC diagnostic pop
 
     const auto cellID = rh.getCellID();
-    const int lid = id_dec != nullptr && !m_layerField.value().empty() ? static_cast<int>(id_dec->get(cellID, layer_idx)) : -1;
-    const int sid = id_dec != nullptr && !m_sectorField.value().empty() ? static_cast<int>(id_dec->get(cellID, sector_idx)) : -1;
+    const int lid     = id_dec != nullptr && !m_layerField.value().empty()
+                            ? static_cast<int>(id_dec->get(cellID, layer_idx))
+                            : -1;
+    const int sid     = id_dec != nullptr && !m_sectorField.value().empty()
+                            ? static_cast<int>(id_dec->get(cellID, sector_idx))
+                            : -1;
     // global positions
     const auto gpos = converter->position(cellID);
     // local positions
+    auto local_det = local;
     if (m_localDetElement.value().empty()) {
-      auto volman = m_geo->detector()->volumeManager();
-      local       = volman.lookupDetElement(cellID & local_mask);
-      }
-      const auto pos = local.nominal().worldToLocal(dd4hep::Position(gpos.x(), gpos.y(), gpos.z()));
-      // cell dimension
-      std::vector<double> cdim;
-      // get segmentation dimensions
-      if (converter->findReadout(local).segmentation().type() != "NoSegmentation") {
+      auto volman = m_geo.detector()->volumeManager();
+      local_det   = volman.lookupDetElement(cellID & local_mask);
+    }
+    const auto pos = local_det.nominal().worldToLocal(dd4hep::Position(gpos.x(), gpos.y(), gpos.z()));
+    // cell dimension
+    std::vector<double> cdim;
+    // get segmentation dimensions
+    if (converter->findReadout(local_det).segmentation().type() != "NoSegmentation") {
       cdim = converter->cellDimensions(cellID);
       // get volume dimensions (multiply by two to get fullsize)
-      } else {
-      // Using bounding box instead of actual solid so the dimensions are always in dim_x, dim_y, dim_z
+    } else {
+      // Using bounding box instead of actual solid so the dimensions are always in dim_x, dim_y,
+      // dim_z
       cdim = converter->findContext(cellID)->volumePlacement().volume().boundingBox().dimensions();
       std::transform(cdim.begin(), cdim.end(), cdim.begin(),
-                      std::bind(std::multiplies<double>(), std::placeholders::_1, 2));
-      }
+                     std::bind(std::multiplies<double>(), std::placeholders::_1, 2));
+    }
 
-      // create const vectors for passing to hit initializer list
-      const decltype(edm4eic::CalorimeterHitData::position) position(
-        gpos.x() / m_lUnit, gpos.y() / m_lUnit, gpos.z() / m_lUnit
-      );
-      const decltype(edm4eic::CalorimeterHitData::dimension) dimension(
-        cdim[0] / m_lUnit, cdim[1] / m_lUnit, cdim[2] / m_lUnit
-      );
-      const decltype(edm4eic::CalorimeterHitData::local) local_position(
-        pos.x() / m_lUnit, pos.y() / m_lUnit, pos.z() / m_lUnit
-      );
+    // create const vectors for passing to hit initializer list
+    const decltype(edm4eic::CalorimeterHitData::position) position(
+        gpos.x() / m_lUnit, gpos.y() / m_lUnit, gpos.z() / m_lUnit);
+    const decltype(edm4eic::CalorimeterHitData::dimension) dimension(
+        cdim[0] / m_lUnit, cdim[1] / m_lUnit, cdim[2] / m_lUnit);
+    const decltype(edm4eic::CalorimeterHitData::local) local_position(
+        pos.x() / m_lUnit, pos.y() / m_lUnit, pos.z() / m_lUnit);
 
-      hits.push_back({
-          rh.getCellID(), // cellID
-          energy,         // energy
-          0,              // @TODO: energy error
-          time,           // time
-          0,              // time error FIXME should be configurable
-          position,       // global pos
-          dimension,
-          // Local hit info
-          sid,
-          lid,
-          local_position, // local pos
-      });
+    hits->push_back({
+        rh.getCellID(), // cellID
+        energy,         // energy
+        0,              // @TODO: energy error
+        time,           // time
+        0,              // time error FIXME should be configurable
+        position,       // global pos
+        dimension,
+        // Local hit info
+        sid, lid,
+        local_position, // local pos
+    });
   }
 }
 
