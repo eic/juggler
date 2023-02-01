e83d43ef586674bc396718fe7022c9ade4923bbd
Sylvester Joosten <sjoosten@anl.gov>
2023-02-01 13:11:24 +0000

    Every named object has an associated ID

--- external/algorithms/core/include/algorithms/name.h
+++ external/algorithms/core/include/algorithms/name.h
@@ -5,6 +5,9 @@
 //
 #pragma once
 
+#include <cstdint>
+#include <functional> // for std::hash
+#include <limits>
 #include <string>
 
 namespace algorithms {
@@ -12,14 +15,26 @@ namespace algorithms {
 // Simple name Mixin providing consistent name API
 class NameMixin {
 public:
+  using IdType = uint32_t;
+
   NameMixin(std::string_view name, std::string_view description)
-      : m_name{name}, m_description{description} {}
+      : m_name{name}, m_description{description}, m_id{calc_hash(name)} {}
   std::string_view name() const { return m_name; }
   std::string_view description() const { return m_description; }
+  IdType id() const { return m_id; }
 
 private:
+  // Helper function to initialize the unique ID based on this entity's name 
+  IdType calc_hash(std::string_view s) {
+    std::hash<std::string_view> hash_alg;
+    const auto fullID = hash_alg(s);
+    const IdType max  = std::numeric_limits<IdType>::max();
+    return static_cast<IdType>(fullID & max);
+  }
+
   const std::string m_name;
   const std::string m_description;
+  const uint32_t m_id;
 };
 
 } // namespace algorithms
