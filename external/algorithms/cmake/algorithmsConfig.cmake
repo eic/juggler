3ec303c7611a8a93e6a111ba17d1d85d3e3a6191
Wouter Deconinck <wdconinc@gmail.com>
2024-02-02 10:49:31 -0600

    fix: rm bundled algorithms instance

--- external/algorithms/cmake/algorithmsConfig.cmake
+++
@@ -1,5 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-# - Include the targets file to create the imported targets that a client can
-# link to (libraries) or execute (programs)
-include("${CMAKE_CURRENT_LIST_DIR}/algorithmsTargets.cmake")
