19f0163148144dbf0e178aa74ecbd7bd0a8f1997
Wouter Deconinck <wdconinc@gmail.com>
2024-07-27 11:27:36 -0400

    fix: remove include files now properly included upstream

--- JugFast/src/components/TruthEnergyPositionClusterMerger.cpp
+++ JugFast/src/components/TruthEnergyPositionClusterMerger.cpp
@@ -2,10 +2,6 @@
 // Copyright (C) 2024 Wouter Deconinck
 
 #include <JugAlgo/Algorithm.h>
-#include <DD4hep/DD4hepUnits.h>
-#include <edm4eic/ClusterCollection.h>
-#include <edm4eic/MCRecoClusterParticleAssociationCollection.h>
-#include <edm4hep/MCParticleCollection.h>
 #include <EICrecon/algorithms/calorimetry/TruthEnergyPositionClusterMerger.h>
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
