55c5a09fc32ba7989c4ecc61882256e8cada6132
Wouter Deconinck <wdconinc@gmail.com>
2025-03-23 10:26:13 -0500

    fix: add ParticleSvc and RandomSvc to AlgoInit preload

--- JugAlgo/src/preload/AlgoInit.cpp
+++ JugAlgo/src/preload/AlgoInit.cpp
@@ -5,6 +5,8 @@
 
 #include <algorithms/geo.h>
 #include <algorithms/service.h>
+#include <algorithms/random.h>
+#include <EICrecon/algorithms/interfaces/ParticleSvc.h>
 
 class AlgoInit {
 private:
@@ -12,18 +14,20 @@ private:
 public:
   AlgoInit() {
     auto detector = dd4hep::Detector::make_unique("");
-    dd4hep::Readout readout(std::string("MockCalorimeterHits"));
-    dd4hep::IDDescriptor id_desc("MockCalorimeterHits", "system:8,layer:8,x:8,y:8");
-    readout.setIDDescriptor(id_desc);
-    detector->add(id_desc);
-    detector->add(readout);
+
+    dd4hep::Readout readoutCalorimeter(std::string("MockCalorimeterHits"));
+    dd4hep::IDDescriptor id_desc_calorimeter("MockCalorimeterHits", "system:8,layer:8,x:8,y:8");
+    dd4hep::Segmentation segmentationCalorimeter("CartesianGridXY","CalorimeterHitsSeg", id_desc_calorimeter.decoder());
+    readoutCalorimeter.setIDDescriptor(id_desc_calorimeter);
+    readoutCalorimeter.setSegmentation(segmentationCalorimeter);
+    detector->add(id_desc_calorimeter);
+    detector->add(readoutCalorimeter);
 
     dd4hep::Readout readoutTracker(std::string("MockTrackerHits"));
     dd4hep::IDDescriptor id_desc_tracker("MockTrackerHits", "system:8,layer:8,x:8,y:8");
-    //Create segmentation with 1x1 mm pixels
-    dd4hep::Segmentation segmentation("CartesianGridXY","TrackerHitsSeg", id_desc_tracker.decoder());
+    dd4hep::Segmentation segmentationTracker("CartesianGridXY","TrackerHitsSeg", id_desc_tracker.decoder());
     readoutTracker.setIDDescriptor(id_desc_tracker);
-    readoutTracker.setSegmentation(segmentation);
+    readoutTracker.setSegmentation(segmentationTracker);
     detector->add(id_desc_tracker);
     detector->add(readoutTracker);
 
@@ -33,6 +37,17 @@ public:
     [[maybe_unused]] auto& geoSvc = algorithms::GeoSvc::instance();
     serviceSvc.setInit<algorithms::GeoSvc>([this](auto&& g) {
       g.init(this->m_detector.get());
+    });
+
+    [[maybe_unused]] auto& particleSvc = algorithms::ParticleSvc::instance();
+    serviceSvc.setInit<algorithms::ParticleSvc>([](auto&& p) {
+      p.init(std::make_shared<algorithms::ParticleSvc::ParticleMap>());
+    });
+
+    [[maybe_unused]] auto& randomSvc = algorithms::RandomSvc::instance();
+    serviceSvc.setInit<algorithms::RandomSvc>([](auto&& r) {
+      r.setProperty("seed", static_cast<size_t>(1));
+      r.init();
     });
 
     serviceSvc.init();
